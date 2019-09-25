376551cdc39843d2091588d250e85ab094ac30be
Domagoj <jogamod@yahoo.com>
2019-09-25 19:12:09 +0200

    Redux branched to feature/client-rest-redux

--- src/renderer/reducers/index.ts
+++ src/renderer/reducers/index.ts
@@ -1,14 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { CounterState, counterReducer } from './counterReducer';
-import { NodeState, restClientReducer } from './restClientReducer';
 
 export interface RootState {
     counter: CounterState;
-    node: NodeState;
 }
 
 export const rootReducer = combineReducers<RootState | undefined>({
-    counter: counterReducer,
-    node: restClientReducer
+    counter: counterReducer
 });
