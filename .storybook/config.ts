7bfdf2edecc3b5703a68e77789ffe9416d0db08f
Belma Gutlic <belma.dzi@gmail.com>
2019-10-08 17:46:46 +0200

    Address PR comment - remove build

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -2,5 +2,11 @@ import { configure } from '@storybook/react';
 
 import '!style-loader!css-loader!sass-loader!../src/renderer/style/index.scss'
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.tsx$/), module);
+// automatically import all files ending in *.stories.tsx
+const req = require.context("../src", true, /.stories.tsx$/);
+
+function loadStories() {
+    req.keys().forEach(req);
+}
+
+configure(loadStories, module);
