c2ec88a46fa31b3d3c670bfb8570d6109f4dad4e
irubido <ivan.rubido@gmail.com>
2019-10-10 15:27:40 +0200

    ButtonPrimitive error

--- src/renderer/components/Button/Button.stories.tsx
+++ src/renderer/components/Button/Button.stories.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from './Button';
+import { ButtonPrimitive } from './Button';
 
-storiesOf('Button', module).add('primitive', () => {
-    return <Button type="primitive">Submit</Button>;
+storiesOf('Button', module).add('Submit', () => {
+    return <ButtonPrimitive >Submit</ButtonPrimitive>;
 });
+
+// storiesOf('Button', module).add('primary', () => {
+//     return <Button type="primary">Submit</Button>;
+// });
+
+// storiesOf('Button', module).add('secondary', () => {
+//     return <Button type="secondary">Submit</Button>;
+// });
+
+// storiesOf('Button', module).add('inverted', () => {
+//     return <Button type="inverted">Submit</Button>;
+// });
+
+// storiesOf('Button', module).add('destructive', () => {
+//     return <Button type="destructive">Submit</Button>;
+// });
--- src/renderer/components/Button/Button.tsx
+++ src/renderer/components/Button/Button.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
 
-interface IProps {
-    type: string;
-    children: any;
+export interface IButtonProps {
+    // type: string;
+    // children: any;
+    disabled: boolean;
+    onClick: () => {};
+}
+export interface IBaseButtonProps extends IButtonProps{
+    buttonType: string;
+}
+const TYPES = {
+    PRIMITIVE: 'primitive',
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary',
+    INVERTED: 'inverted',
+    DESTRUCTIVE: 'destructive',
 }
 
-export const Button: React.FunctionComponent<IProps> = ({type, children, ...props}) => (
-    <button className={`button btn-${type}`} {...props}>
-        {children}
-    </button>
-);
+const BaseButton: React.FunctionComponent<IBaseButtonProps> = ({
+    children,
+    disabled,
+    onClick,
+    buttonType,
+}) => (<button className={buttonType} disabled={disabled} onClick={onClick}>{children}</button>);
+
+export const ButtonPrimitive: React.FunctionComponent<React.PropsWithChildren<IButtonProps>> = ({children, disabled, onClick}) => {
+    return(<BaseButton disabled={disabled} onClick={onClick} buttonType={TYPES.PRIMITIVE}>{children}</BaseButton>)
+    
+};
+
