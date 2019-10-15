9e93b01b4ce3133592d6fff33365b0e424efb245
Marin Petrunić <marin.petrunic@gmail.com>
2019-10-15 14:44:44 +0200

    add chrome driver opts

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -12,6 +12,10 @@ describe("Main window", () => {
                 path: path.join(__dirname, "../../node_modules/.bin/electron"),
                 args: [path.join(__dirname, "..", "..")],
                 startTimeout: 10000,
+                chromeDriverArgs: [
+                    "--no-sandbox",
+                    "--disable-dev-shm-usage"
+                ]
             });
             await app.start();
         } catch (e) {
