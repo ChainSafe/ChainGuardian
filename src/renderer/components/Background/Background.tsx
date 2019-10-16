0f5a4364a26b50fbeffc098eeb6de0731408c023
irubido <ivan.rubido@gmail.com>
2019-10-16 14:08:52 +0200

    fix

--- src/renderer/components/Background/Background.tsx
+++ src/renderer/components/Background/Background.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 
-interface IProps {
+interface IBackgroundProps {
     children?: any;
     basic?: boolean;
 }
 
-export const Background: React.FunctionComponent<React.PropsWithChildren<IProps>> = ({
+export const Background: React.FunctionComponent<React.PropsWithChildren<IBackgroundProps>> = ({
     children, 
     basic}) => ( basic ? 
     <div className="background">
         <div className="children">{children}</div>
-    </div> : <div className="background">
+    </div> 
+    : 
+    <div className="background">
         <div className="illustration" >
             <div className="children">{children}</div>
         </div>
