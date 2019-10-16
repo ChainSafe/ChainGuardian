88d9abc4a33e8e56bf7c444f34ae09116fe538cf
Marin Petrunić <marin.petrunic@gmail.com>
2019-10-16 10:18:11 +0200

    add chrome driver opts

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -12,22 +12,7 @@ describe("Main window", () => {
                 path: path.join(__dirname, "../../node_modules/.bin/electron"),
                 args: [path.join(__dirname, "..", "..")],
                 startTimeout: 10000,
-                chromeDriverArgs: [
-                    "--no-sandbox",
-                    "--disable-dev-shm-usage",
-                    "--headless",
-                    "no-sandbox",
-                    "disable-dev-shm-usage",
-                    "headless"
-                ],
-                webdriverOptions: [
-                    "--no-sandbox",
-                    "--disable-dev-shm-usage",
-                    "--headless",
-                    "no-sandbox",
-                    "disable-dev-shm-usage",
-                    "headless"
-                ]
+                chromeDriverArgs: ["no-sandbox", "headless", "disable-extensions", "disable-infobars"]
             });
             await app.start();
         } catch (e) {
