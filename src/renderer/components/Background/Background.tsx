5be0e2a028bbfb0c8c66329b397988206101745c
irubido <ivan.rubido@gmail.com>
2019-10-16 11:08:06 +0200

    background component, added basic prop

--- src/renderer/components/Background/Background.tsx
+++ src/renderer/components/Background/Background.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 
 interface IProps {
     children?: any;
+    basic?: boolean;
 }
 
-export const Background: React.FunctionComponent<React.PropsWithChildren<IProps>> = ({children}) => (
+export const Background: React.FunctionComponent<React.PropsWithChildren<IProps>> = ({
+    children, 
+    basic}) => ( basic ? 
     <div className="background">
-        {children}
+        <div className="children">{children}</div>
+    </div> : <div className="background">
+        <div className="illustration" >
+            <div className="children">{children}</div>
+        </div>
     </div>
 );
