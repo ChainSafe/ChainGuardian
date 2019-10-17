fb46aad8cf77e92df5278f9e09a1f4faab82257a
irubido <ivan.rubido@gmail.com>
2019-10-17 15:19:30 +0200

    label, validation style props, fonts

--- src/renderer/components/Input/InputForm.stories.tsx
+++ src/renderer/components/Input/InputForm.stories.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
 import { InputForm} from './InputForm';
+import { string } from 'prop-types';
 
 storiesOf('Input Form', module).add('Input Form', () => {
-    return  <InputForm />;
+    const options = {
+        gray: "",
+        success: true,
+        error: false,
+    }
+    const defaultValue = undefined;
+    const label = text("label text", "Input")
+    const value = select("valid", options, defaultValue,);
+    const eMessage = text("error message", "Error message");
+    return  <InputForm valid={value} label={label} errorMessage={eMessage}/>;
 }).addDecorator(withKnobs);
--- src/renderer/components/Input/InputForm.tsx
+++ src/renderer/components/Input/InputForm.tsx
@@ -1,20 +1,37 @@
 import * as React from "react";
 import {useState} from "react";
 
-export interface IInputFormProps  {
-    text?: string;
+export interface IInputFormProps {
     label?: string;
     valid?: boolean;
-    error?: string;
+    errorMessage?: string;
+    input?: string;
 }
 
-export const InputForm: React.FunctionComponent<IInputFormProps> = (initalData) => {
-    const [input, setInput] = useState<IInputFormProps>(initalData)
+export const InputForm: React.FunctionComponent<IInputFormProps> = (props: IInputFormProps) => {
+    const [input, setInput] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value);
+    };
+
+    const classNamesValid = (props: boolean | undefined) => {
+        switch(props) {
+            case undefined : return("");
+            case true : return("success");
+            case false : return("error");
+        }
+    };
     
     return(
         <form>
-            <label></label>
-            <input className="inputform" onChange={e => setInput({...input, text: e.target.value})} />
+            <div className="label">{props.label}</div>
+            <input 
+                className={`inputform ${classNamesValid(props.valid)}`} 
+                onChange={handleInputChange} />
+            <div 
+                className={`error-message ${(classNamesValid(props.valid) !== "error") ? "none" : "" }`}>
+                {props.errorMessage}</div>
         </form>
     );
 };
