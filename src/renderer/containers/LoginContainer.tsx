28489d55a2f8b0ae1286ae72c41a469d4924eb60
irubido <ivan.rubido@gmail.com>
2019-10-21 15:14:41 +0200

    LoginContainer, multiple components css refactored, svg fix

--- src/renderer/containers/LoginContainer.tsx
+++ src/renderer/containers/LoginContainer.tsx
@@ -1,10 +1,24 @@
 import * as React from "react";
 import {Background} from "../components/Background/Background";
+import {Modal} from "../components/Modal/Modal";
+import {InputForm} from "../components/Input/InputForm";
+import {ButtonPrimary, ButtonSecondary} from "../components/Button/ButtonStandard";
 
 export default class LoginContainer extends React.Component {
     render() {
         return (
-            <Background></Background>
+            <Background>
+                <Modal>
+                    <h1>Welcome!</h1>
+                    <p>Please enter your password or set up an account to get started.</p>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <InputForm />
+                        <ButtonSecondary>GO</ButtonSecondary>
+                    </div>
+                    <h5>OR</h5>
+                    <ButtonPrimary>REGISTER</ButtonPrimary>
+                </Modal>
+            </Background>
         );
     }
 }
