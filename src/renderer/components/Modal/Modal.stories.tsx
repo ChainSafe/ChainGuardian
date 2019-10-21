fa1b9a8e063a1aa018ef81eb310654f2012be17f
irubido <ivan.rubido@gmail.com>
2019-10-21 09:28:30 +0200

    modal inline css

--- src/renderer/components/Modal/Modal.stories.tsx
+++ src/renderer/components/Modal/Modal.stories.tsx
@@ -7,7 +7,9 @@ import { ButtonPrimitive } from '../Button/ButtonStandard';
 storiesOf('Modal', module).add('Modal', () => {
     const backButton = boolean('back button', false);
     
-    return  <Modal hasBack={backButton}>
-        <ButtonPrimitive>BUTTON</ButtonPrimitive>
-        </Modal>;
+    return <div style={{display: "flex", justifyContent: "center"}}>
+            <Modal hasBack={backButton}>
+                <ButtonPrimitive>BUTTON</ButtonPrimitive>
+            </Modal>
+        </div>;
 }).addDecorator(withKnobs);
