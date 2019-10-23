dde71131db4727d0c0507c3a2a0cb9340862ef6f
irubido <ivan.rubido@gmail.com>
2019-10-23 12:39:16 +0200

    LoginContainer state fix, input form focused prop

--- src/renderer/components/Input/InputForm.tsx
+++ src/renderer/components/Input/InputForm.tsx
@@ -1,22 +1,17 @@
 import * as React from "react";
-import {useState} from "react";
 
 export interface IInputFormProps {
     label?: string;
     valid?: boolean;
     errorMessage?: string;
-    inputValue?: string;
+    inputValue?: any;
     placeholder?: string;
+    onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
+    focused?: boolean;
 }
 
 export const InputForm: React.FunctionComponent<IInputFormProps> = (props: IInputFormProps) => {
-    const [input, setInput] = useState("");
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setInput(e.currentTarget.value);
-        props.inputValue = input;
-    };
-
+    
     const classNamesValid = (props: boolean | undefined): string => {
         switch(props) {
             case undefined : return("");
@@ -25,14 +20,27 @@ export const InputForm: React.FunctionComponent<IInputFormProps> = (props: IInpu
         }
     };
     
-    return(
+    return( props.focused ?
+        <form>
+            <div className="label">{props.label}</div>
+            <input 
+                ref={(input): any  => input && input.focus()}
+                placeholder={props.placeholder}
+                value={props.inputValue}
+                className={`inputform ${classNamesValid(props.valid)}`} 
+                onChange={props.onChange} />
+            <div 
+                className={`error-message ${(classNamesValid(props.valid) !== "error") ? "none" : "" }`}>
+                {props.errorMessage}</div>
+        </form>
+        :
         <form>
             <div className="label">{props.label}</div>
             <input 
                 placeholder={props.placeholder}
                 value={props.inputValue}
                 className={`inputform ${classNamesValid(props.valid)}`} 
-                onChange={handleInputChange} />
+                onChange={props.onChange} />
             <div 
                 className={`error-message ${(classNamesValid(props.valid) !== "error") ? "none" : "" }`}>
                 {props.errorMessage}</div>
--- src/renderer/containers/LoginContainer.tsx
+++ src/renderer/containers/LoginContainer.tsx
@@ -4,8 +4,19 @@ import {Modal} from "../components/Modal/Modal";
 import {InputForm} from "../components/Input/InputForm";
 import {ButtonPrimary, ButtonSecondary} from "../components/Button/ButtonStandard";
 
+interface IState {
+    input: string;
+}
 
 export default class LoginContainer extends React.Component {
+
+    state: IState = {
+        input: ""
+    };
+    handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+        this.setState({input: e.currentTarget.value});
+    };
+    
     render(): any {
         return (
             <Background>
@@ -13,7 +24,10 @@ export default class LoginContainer extends React.Component {
                     <h1>Welcome!</h1>
                     <p>Please enter your password or set up an account to get started.</p>
                     <div className="input-container">
-                        <InputForm placeholder="Enter password" />
+                        <InputForm 
+                            focused onChange={this.handleChange} 
+                            inputValue={this.state.input} 
+                            placeholder="Enter password" /> 
                         <ButtonSecondary>GO</ButtonSecondary>
                     </div>
                     <h5>OR</h5>
@@ -22,4 +36,5 @@ export default class LoginContainer extends React.Component {
             </Background>
         );
     }
+    
 }
