100ce44238f9e393d41afc62ba474483d9db11c2
irubido <ivan.rubido@gmail.com>
2019-10-25 11:18:19 +0200

    step navigation component

---
+++ src/renderer/components/StepNavigation/StepNavigation.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export interface IStepNavigationProps {
+    steps: Array<{stepId: number, stepName: string}>;
+    current: number;
+}
+
+export const StepNavigation: React.FunctionComponent<IStepNavigationProps> = (props: IStepNavigationProps) => {
+
+    return(
+        <div className="step-container">
+            {props.steps.map(n => {
+                return <div 
+                    className={`step ${props.current >= n.stepId ? "current" : ""}`} 
+                    key={n.stepId}>
+                    {n.stepName}
+                </div>;
+            })};
+        </div>
+    );
+};
