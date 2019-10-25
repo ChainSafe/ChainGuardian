a1ea823e8af12e5b3a855c5070c53befee5a1cc9
mak <mak@nodefactory.io>
2019-10-25 14:08:39 +0200

    Fix deposit util unit tests

--- test/unit/Services/deposit/utils.spec.ts
+++ test/unit/Services/deposit/utils.spec.ts
@@ -1,19 +1,19 @@
-import { functionSignatureFromABI } from '../../../../src/renderer/services/deposit/utils';
+import { functionSignatureFromABI } from '../../../../src/renderer/services/deposit';
 import options from "../../../../src/renderer/services/deposit/options";
 
 describe("deposit utils unit test", () => {
     it("should successfully extract deposit function signature.", async () => {
-        expect(functionSignatureFromABI(options.depositContract.abi, "deposit"))
+        expect(functionSignatureFromABI(options.abi, "deposit"))
             .toBe("deposit(bytes,bytes,bytes)");
     });
 
     it("should successfully extract get_hash_tree_root function signature.", async () => {
-        expect(functionSignatureFromABI(options.depositContract.abi, "get_hash_tree_root"))
+        expect(functionSignatureFromABI(options.abi, "get_hash_tree_root"))
             .toBe("get_hash_tree_root()");
     });
 
     it("should fail to extract signature from nonexistent function.", async () => {
-        expect(functionSignatureFromABI(options.depositContract.abi, "no_function"))
+        expect(functionSignatureFromABI(options.abi, "no_function"))
             .toBe("");
     });
 });
