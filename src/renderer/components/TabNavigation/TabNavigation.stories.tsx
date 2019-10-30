3825640411fb64ece34daa30ac21b13008ac1bc2
irubido <ivan.rubido@gmail.com>
2019-10-30 10:03:00 +0100

    tab navigation onTab prop

--- src/renderer/components/TabNavigation/TabNavigation.stories.tsx
+++ src/renderer/components/TabNavigation/TabNavigation.stories.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import {useState} from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, number, select, object } from '@storybook/addon-knobs';
 import { TabNavigation } from "./TabNavigation";
 
 storiesOf('Tab Navigation', module).add('Tab Navigation', () => {
-    const selected= number("current", 1,);
+    const [selected, setSelected]=useState(1);
+
+    // const selected= number("current", 1,);
     const d = [
         {
             tabId: 1,
@@ -21,6 +24,6 @@ storiesOf('Tab Navigation', module).add('Tab Navigation', () => {
     ];
     const array = object("steps", d);
     return <div >
-            <TabNavigation tabs={array} current={selected}/>
+            <TabNavigation onTab={()=>setSelected(selected)} tabs={array} current={selected}/>
         </div>;
 }).addDecorator(withKnobs);
--- src/renderer/components/TabNavigation/TabNavigation.tsx
+++ src/renderer/components/TabNavigation/TabNavigation.tsx
@@ -3,15 +3,16 @@ import * as React from "react";
 export interface ITabNavigationProps {
     tabs: Array<{tabId: number, tabName: string}>;
     current: number;
+    onTab?: (id: number) => void;
 }
 
 export const TabNavigation: React.FunctionComponent<ITabNavigationProps> = (props: ITabNavigationProps) => {
-
     return(
         <div className="tab-container">
             {props.tabs.map(n => {
                 return <div 
-                    className={`tab ${props.current === n.tabId ? "current-tab" : ""}`} 
+                    onClick={() => props.onTab && props.onTab(n.tabId) }
+                    className={`tab ${props.current === n.tabId ? "current-tab" : ""}`}
                     key={n.tabId}>
                     {n.tabName}
                 </div>;
