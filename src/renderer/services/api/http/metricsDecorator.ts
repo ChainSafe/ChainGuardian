afe96f6884b793840430a766d1c4c856bef10e29
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2019-10-31 21:05:00 +0100

    Added comments

--- src/renderer/services/api/http/metricsDecorator.ts
+++ src/renderer/services/api/http/metricsDecorator.ts
@@ -1,3 +1,17 @@
+/**
+ * Decorator that allows us to track metrics of any method.
+ * It measures the execution time and persists this metric.
+ * 
+ * Example usage:
+ * 
+ * class Example{
+ *  
+ *  @trackMetrics()
+ *  method(){
+ *      console.log("Do the hard work");
+ *  }
+ * }
+ */
 export function trackMetrics(): MethodDecorator {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor): any {
         let descriptorCopy = descriptor;
@@ -6,14 +20,22 @@ export function trackMetrics(): MethodDecorator {
             descriptorCopy = Object.getOwnPropertyDescriptor(target, propertyKey);
         }
         const originalMethod = descriptorCopy.value;
+        // Counter to track the number of times the method was executed
         let i = 0;
         let startTime = 0, endTime = 0;
         descriptorCopy.value = function (...args: any[]): any {
             const id = `${String(propertyKey)}_${i++}`;
+            // Save the time before the method execution
             startTime = performance.now();
+            // Call the original method
             const result = originalMethod.apply(this, args);
+            // Save the time after the method executed
             endTime = performance.now();
 
+            /**
+             * TODO: Implement metrics persistance
+             * Save this "log" data into the DB
+             */
             console.log(`method "${id}" took ${endTime - startTime}ms`);
             console.log(`\t args: ${JSON.stringify(args)}`);
             return result;
