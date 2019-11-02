89eb4d60912fe951318f1d57fab6fbe0d50851de
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2019-11-02 01:13:41 +0100

    Added babel support for decorators

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -31,6 +31,7 @@ module.exports = merge.smart(baseConfig, {
                         '@babel/preset-react'
                     ],
                     plugins: [
+                        ["@babel/plugin-proposal-decorators", { "legacy": true }],
                         ['@babel/plugin-proposal-class-properties', { loose: true }]
                     ]
                 }
