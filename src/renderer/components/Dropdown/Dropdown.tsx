81525e10fbd0054ccb50f49b02ecd0cd7614729a
irubido <ivan.rubido@gmail.com>
2019-11-13 16:10:50 +0100

    dropdown component

---
+++ src/renderer/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import {useState} from "react";
+
+export interface IDropdownProps {
+    options: Array<string>;
+    current: number;
+    onChange: (selected: number) => void;
+}
+
+export const Dropdown: React.FunctionComponent<IDropdownProps> = (props: IDropdownProps) => {
+    const [visible, setVisible]=useState("none");
+
+    function getSelectedIndex(clickedOption: string):number{
+        return (props.options.findIndex(option => option === clickedOption));
+    }
+    function showHide():void{
+        visible === "none" ? setVisible("block") : setVisible("none");
+    }
+    function hide():void{
+        visible === "block" ? setVisible("none") : null;
+    }
+    
+    return(
+        <div onClick={()=> hide()} className="dropdown-screen">
+            <div className="dropdown-container">
+                <div onClick={()=> showHide()} className="dropdown-selected">
+                    {props.options[props.current]}
+                </div>
+                <div className="dropdown-items">
+                {props.options.map(option =>{
+                    return <div 
+                        key={option} 
+                        onClick={() => {props.onChange(getSelectedIndex(option));showHide();}} 
+                        className={`dropdown-item 
+                        ${visible}
+                        ${getSelectedIndex(option)===props.current?"selected":""}`} 
+                        >{option}</div>
+                })}
+                </div>
+            </div>
+        </div>
+    );
+};
