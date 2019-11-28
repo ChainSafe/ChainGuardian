3ac66718625ddfc6805302ffcef397aa8bbc2b1a
Marin Petrunić <marin.petrunic@gmail.com>
2019-11-28 10:11:42 +0100

    validation  utils

--- src/renderer/containers/Onboard/CreatePassword/validation.ts
+++ src/renderer/containers/Onboard/CreatePassword/validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from "@hapi/joi";
 import passwordComplexity from "../../../services/validation/password-complexity";
 
-export const passwordFormSchema = Joi.object({
+export const passwordFormSchema = Joi.object().keys({
     password: passwordComplexity().required(),
-    confirm: Joi.string().valid(Joi.ref("password")),
-});
+    confirm: Joi.string().valid(Joi.ref("password")).messages({"any.only": "Passwords are not equal"}),
+}).with("password", "confirm");
