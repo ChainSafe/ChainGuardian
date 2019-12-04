5937c385dee2785df9192cc4adb05fb24504ed0c
mak <mak@nodefactory.io>
2019-12-04 08:29:24 +0100

    Code cleanup

--- src/renderer/services/validation/util.ts
+++ src/renderer/services/validation/util.ts
@@ -19,25 +19,3 @@ export function joiValidationToErrorMessages(joiError: ValidationError): IValida
     }
     return errors;
 }
-
-export interface IValidationErrorsDetails {
-    [k: string]: {[t: string]: string[]};
-}
-
-export function joiValidationToErrorDetailsMessages(joiError: ValidationError): IValidationErrorsDetails {
-    const errors: {[k: string]: {[t: string]: string[]}} = {};
-    if (joiError) {
-        joiError.details.forEach(function(detail) {
-            const baseType = detail.type.split(".")[0];
-            detail.path.forEach((path) => {
-                if (!errors[path]) errors[path] = {};
-                if(!(errors[path])[baseType]) {
-                    (errors[path])[baseType] = [detail.message];
-                } else {
-                    (errors[path])[baseType].push(detail.message);
-                }
-            });
-        });
-    }
-    return errors;
-}
