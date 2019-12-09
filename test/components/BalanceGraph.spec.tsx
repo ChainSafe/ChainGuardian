de2241f3d821ee82438b502305524b44e8374c05
irubido <ivan.rubido@gmail.com>
2019-12-09 13:15:57 +0100

    lint fix

--- test/components/BalanceGraph.spec.tsx
+++ test/components/BalanceGraph.spec.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import * as renderer from "react-test-renderer";
 import {BalanceGraph, IntervalEnum} from "../../src/renderer/components/BalanceGraph/BalanceGraph";
 
-const giveData = async () => {
+const giveData = async (): Promise<number[]> => {
     return [2356,3213,8934,7924,7924,1245,3456,5673,2124,
         1002,4143,4143,4143,4143,3234,3245,
         4325,2333,5673,4623,1982,6753,5432,3463,
         1245,3341,2221,2234,3255,3255,3255,6544,
         5522,5522,5522,5522,5522,5522,5522,5522,
         4621,3111,1255,3220,2000,4335,5654,3245,3453,
-        6777,6777,6777,6777,6777,6777,4000,4000,4000,1000,1200]
-}
+        6777,6777,6777,6777,6777,6777,4000,4000,4000,1000,1200];
+};
 
 describe("BalanceGraph", () => {
     it("renders correctly, hour", () => {
@@ -18,7 +18,7 @@ describe("BalanceGraph", () => {
             .create(<BalanceGraph 
                 defaultInterval={IntervalEnum.HOUR}
                 getData={giveData}
-                />)
+            />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
@@ -27,7 +27,7 @@ describe("BalanceGraph", () => {
             .create(<BalanceGraph 
                 defaultInterval={IntervalEnum.DAY}
                 getData={giveData}
-                />)
+            />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
@@ -36,7 +36,7 @@ describe("BalanceGraph", () => {
             .create(<BalanceGraph 
                 defaultInterval={IntervalEnum.WEEK}
                 getData={giveData}
-                />)
+            />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
@@ -45,7 +45,7 @@ describe("BalanceGraph", () => {
             .create(<BalanceGraph 
                 defaultInterval={IntervalEnum.MONTH}
                 getData={giveData}
-                />)
+            />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
@@ -54,7 +54,7 @@ describe("BalanceGraph", () => {
             .create(<BalanceGraph 
                 defaultInterval={IntervalEnum.YEAR}
                 getData={giveData}
-                />)
+            />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
