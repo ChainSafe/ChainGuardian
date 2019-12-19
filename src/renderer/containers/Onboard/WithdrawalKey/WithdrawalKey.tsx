1565f9917679e76a2673b1737fe1d6009f9eeea6
morrigan <belma.dzi@gmail.com>
2019-12-19 14:53:25 +0100

    Add skip option in withdrawal screen

--- src/renderer/containers/Onboard/WithdrawalKey/WithdrawalKey.tsx
+++ src/renderer/containers/Onboard/WithdrawalKey/WithdrawalKey.tsx
@@ -1,16 +1,16 @@
 import React, {ReactElement} from "react";
 import {Link} from "react-router-dom";
-import {ButtonSecondary, ButtonPrimary} from "../../../components/Button/ButtonStandard";
+import {ButtonSecondary, ButtonPrimary,ButtonInverted} from "../../../components/Button/ButtonStandard";
 import {Routes, OnBoardingRoutes} from "../../../constants/routes";
 
 export default function WithdrawalKey(): ReactElement {
     return (
         <>
-            <h1>Enter your withdrawal key</h1>
+            <h1>Get a withdrawal key</h1>
             <p>In case your signing key gets compromised,
                 all of your validator balance will be withdrawn to this wallet.
                 Once a deposit is submitted, you cannot change this wallet.
-                We suggest storing this separately fro the signing key</p>
+                We suggest storing this separately for the signing key.</p>
             <div className="action-buttons">
                 <Link to={Routes.ONBOARD_ROUTE_EVALUATE(OnBoardingRoutes.WITHDRAWAL_IMPORT)}>
                     <ButtonSecondary buttonId="import" large>IMPORT</ButtonSecondary>
@@ -18,6 +18,13 @@ export default function WithdrawalKey(): ReactElement {
 
                 <ButtonPrimary buttonId="generate" large>GENERATE</ButtonPrimary>
             </div>
+
+            <h5 className="input-or">OR</h5>
+
+            <Link to={Routes.ONBOARD_ROUTE_EVALUATE(OnBoardingRoutes.DEPOSIT_TX)}>
+                <ButtonInverted>SKIP</ButtonInverted>
+            </Link>
+            <p></p>
         </>
     );
 }
