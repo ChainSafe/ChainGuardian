fb43572eea9b4c0ad5ff8c271a58ebb75ccac5f8
irubido <ivan.rubido@gmail.com>
2019-12-30 11:36:55 +0100

    draft

--- src/renderer/containers/Login/LoginContainer.tsx
+++ src/renderer/containers/Login/LoginContainer.tsx
@@ -1,28 +1,58 @@
 import * as React from "react";
-import {ReactNode} from "react";
+import {ReactElement, Component} from "react";
 import {Background} from "../../components/Background/Background";
 import {Modal} from "../../components/Modal/Modal";
 import {InputForm} from "../../components/Input/InputForm";
 import {ButtonPrimary, ButtonSecondary} from "../../components/Button/ButtonStandard";
 import {Link} from "react-router-dom";
 import {Routes, OnBoardingRoutes} from "../../constants/routes";
+import {CGAccount} from "../../models/account";
+import {RouteComponentProps} from "react-router";
+import {Notification} from "../../components/Notification/Notification";
+import {Level, Horizontal, Vertical} from "../../components/Notification/NotificationEnums";
 
 interface IState {
     input: string;
+    notification: boolean;
 }
 
-export default class LoginContainer extends React.Component {
+export default class LoginContainer extends Component<RouteComponentProps> {
     public state: IState = {
-        input: ""
+        input: "",
+        notification: false
     };
-    public handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    private handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
         this.setState({input: e.currentTarget.value});
     };
     
-    public render(): ReactNode {
+    private handleSubmit = (): void => {
+        const account = new CGAccount({
+            name: "",
+            directory: "",
+            sendStats: false
+        });
+        console.log(account.isCorrectPassword(this.state.input));
+        if(account.isCorrectPassword(this.state.input)){
+            this.props.history.push(Routes.DASHBOARD_ROUTE);
+        } else {
+            this.setState({notification: true});
+        }
+    }
+
+    public render(): ReactElement {
         return (
             <Background>
                 <Modal>
+                    <Notification
+                        title="Incorrect password"
+                        isVisible={this.state.notification}
+                        level={Level.ERROR}
+                        horizontalPosition={Horizontal.CENTER}
+                        verticalPosition={Vertical.TOP}
+                        onClose={(): void => {this.setState({notification: false});}}
+                    >
+                    Try again.
+                    </Notification>
                     <h1>Welcome!</h1>
                     <p>Please enter your password or set up an account to get started.</p>
                     <div className="input-container">
@@ -32,9 +62,10 @@ export default class LoginContainer extends React.Component {
                             inputValue={this.state.input} 
                             placeholder="Enter password"
                         />
-                        <Link to={Routes.DASHBOARD_ROUTE}>
-                            <ButtonSecondary buttonId="go" >GO</ButtonSecondary>
-                        </Link>
+                        <ButtonSecondary
+                            buttonId="go"
+                            onClick={(): void => {this.handleSubmit()}}
+                        >GO</ButtonSecondary>
                     </div>
                     <h5 className="input-or">OR</h5>
                     <Link to={Routes.ONBOARD_ROUTE_EVALUATE(OnBoardingRoutes.SIGNING)}>
