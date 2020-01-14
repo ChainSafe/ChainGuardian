8a94f09efec39cb4292aa2279ea4a02868f3fb74
irubido <ivan.rubido@gmail.com>
2020-01-14 15:03:13 +0100

    Validators from db, prompt password from db

--- src/renderer/containers/Dashboard/DashboardContainer.tsx
+++ src/renderer/containers/Dashboard/DashboardContainer.tsx
@@ -11,12 +11,18 @@ import {CGAccount} from "../../models/account";
 import database from "../../services/db/api/database";
 import { DEFAULT_ACCOUNT } from "../../constants/account";
 import {Loading} from "../../components/Loading/Loading";
+import {connect} from "react-redux";
+import {IRootState} from "../../reducers/index";
+import {RouteComponentProps} from "react-router";
+import {Routes} from "../../constants/routes";
 
-interface IDashboardProps {
-    validators: Array<IValidator>;
-    currentNetwork: number;
-    notification: INotificationState;
-}
+// interface IDashboardProps {
+//     validators: Array<IValidator>;
+//     currentNetwork: number;
+//     notification: INotificationState;
+// }
+
+type IOwnProps = Pick<RouteComponentProps, "history">;
 
 interface INotificationState {
     title?: string;
@@ -34,7 +40,7 @@ export interface IValidator {
     password: string;
 }
 
-export const DashboardContainer: React.FunctionComponent<IDashboardProps> = () => {
+const Dashboard: React.FunctionComponent<IOwnProps &  Pick<IRootState, "auth">> = (props) => {
     
     // TODO - temporary object, import real network object
     const networksMock: {[id: number]: string} = {
@@ -49,6 +55,7 @@ export const DashboardContainer: React.FunctionComponent<IDashboardProps> = () =
     const [validators, setValidators] = useState<Array<IValidator>>([]);
     const [currentNetwork, setCurrentNetwork] = useState<number>(0);
     const [notification, setNotification] = useState<INotificationState>(HiddenNotification);
+
 
     const onAddNewValidator = (): void => {
         // TODO - implement
@@ -78,37 +85,7 @@ export const DashboardContainer: React.FunctionComponent<IDashboardProps> = () =
         }
     };
 
-    const getValidators = (): Array<IValidator> => {
-        // TODO - call real validator fetch
-
-        return [{
-            name: "V1",
-            status: "Working",
-            publicKey: "0x32rzhcc44e690220c190be41378baf6152560eb13fa73bdf8b45120b56096acc4b4e87a0e0b97f83e48f0ff499",
-            deposit: 30,
-            network: "NetworkA",
-            privateKey: "0xe68ffdb8b9729cb02c5be506e9a2fad086746b4bdc2f50fb74d10ac8419c5259",
-            password: "mock"
-        },{
-            name: "V2",
-            status: "Not Working",
-            publicKey: "0x22rrrcc44e690220c190be41378baf6152560eb13fa73bdf8b45120b56096acc4b4e87a0e0b97f83e48f0ff499",
-            deposit: 30,
-            network: "NetworkA",
-            privateKey: "0x92fffcc44e690220c190be41378baf6152560eb13fa73bdf8b45120b56096acc4b4e87a0e0b97f83e48f0ff499",
-            password: "mock"
-        },{
-            name: "V3",
-            status: "Not Working",
-            publicKey: "0x1d32a7822345564",
-            deposit: 30,
-            network: "NetworkB",
-            privateKey: "0x62dddcc44e690220c190be41378baf6152560eb13fa73bdf8b45120b56096acc4b4e87a0e0b97f83e48f0ff499s",
-            password: "mock"
-        }];
-    }
-
-    const getValidatorsData = async (): Promise<void> => {
+    const getValidators = async (): Promise<void> => {
         const validatorArray = [];
 
         const validatorsData = await database.account.get(DEFAULT_ACCOUNT);
@@ -139,7 +116,8 @@ export const DashboardContainer: React.FunctionComponent<IDashboardProps> = () =
     }
 
     useEffect(()=>{
-        getValidatorsData();
+        if(!props.auth.auth) props.history.push(Routes.LOGIN_ROUTE);
+        getValidators();
     },[]);
 
     const topBar =
@@ -193,6 +171,15 @@ export const DashboardContainer: React.FunctionComponent<IDashboardProps> = () =
     );
 }
 
+const mapStateToProps = (state: IRootState): Pick<IRootState, "auth"> => ({
+    auth: state.auth
+});
+
+
+export const DashboardContainer = connect(
+    mapStateToProps,
+    null
+)(Dashboard);
 
 // export default class DashboardContainer extends React.Component {
 
