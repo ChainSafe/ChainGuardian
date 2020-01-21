5c82891f23529a83b832d6b8397edd4f1656654b
irubido <ivan.rubido@gmail.com>
2020-01-21 10:11:24 +0100

    draft

--- src/renderer/containers/Onboard/SigningKey/Verify/SigningKeyVerify.tsx
+++ src/renderer/containers/Onboard/SigningKey/Verify/SigningKeyVerify.tsx
@@ -13,30 +13,53 @@ import {PrivateKey} from "@chainsafe/bls/lib/privateKey";
 import {DEFAULT_ACCOUNT} from "../../../../constants/account";
 import {Keypair} from "@chainsafe/bls/lib/keypair";
 import database from "../../../../services/db/api/database";
+import {getConfig} from "../../../../../config/config";
+import * as path from "path";
+import {remote} from "electron";
+import {V4Keystore} from "../../../../services/keystore";
+import {CheckPassword} from "../../../../components/CheckPassword/CheckPassword";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IOwnProps extends Pick<RouteComponentProps, "history"> {
 
 }
 
+export interface IState {
+    showPrompt: boolean
+} 
+
 class SigningMnemonicQuestion extends Component<IOwnProps &  Pick<IRootState, "register" | "addValidator" | "auth"> & IInjectedProps, {}> {
+
+    public state: IState = {
+        showPrompt: false
+    }
+
     public render(): ReactElement {
         const mnemonic = this.props.register.signingMnemonic.split(" ");
         const randArray = getRandomIntArray(12);
         const correctAnswerIndex = randArray[getRandomInt(3)];
         return (
-            <VerifyMnemonic
-                question={`What’s the ${ordinalSuffix(correctAnswerIndex+1)} word in the mnemonic?`}
-                answers={[mnemonic[randArray[0]], mnemonic[randArray[1]], mnemonic[randArray[2]]]}
-                correctAnswer={mnemonic[correctAnswerIndex]}
-                onCorrectAnswer={(): void => {setTimeout(this.handleCorrectAnswer, 1000);}}
-                onInvalidAnswer={(): void => {setTimeout(this.handleInvalidAnswer, 1000);}}
-            />
+            <>
+                <CheckPassword
+                    showPrompt={this.state.showPrompt}
+                    onCorrectPassword={this.handleCorrectAnswer}
+                />
+                <VerifyMnemonic
+                    question={`What’s the ${ordinalSuffix(correctAnswerIndex+1)} word in the mnemonic?`}
+                    answers={[mnemonic[randArray[0]], mnemonic[randArray[1]], mnemonic[randArray[2]]]}
+                    correctAnswer={mnemonic[correctAnswerIndex]}
+                    onCorrectAnswer={(): void => {setTimeout(this.handleCorrectAnswer, 1000);}}
+                    onInvalidAnswer={(): void => {setTimeout(this.handleInvalidAnswer, 1000);}}
+                />
+            </>
         );
     }
 
     private handleCorrectAnswer = async (): Promise<void> => {
 
+        /**
+         * In case Add Validator button was pressed on Dashboard
+         */
         if(this.props.addValidator.addValidator){
             
             const signingKeyString = Eth2HDWallet.getKeypair(this.props.register.signingMnemonic).privateKey.toHexString();
@@ -46,18 +69,27 @@ class SigningMnemonicQuestion extends Component<IOwnProps &  Pick<IRootState, "r
             
             if(this.props.auth.auth !== null) {
                 this.props.auth.auth.addValidator(new Keypair(signingKey));
-                const y = await database.account.get(DEFAULT_ACCOUNT);
+                
+                const accountDirectory = path.join(getConfig(remote.app).storage.accountsDir, DEFAULT_ACCOUNT);
 
-                await database.account.set(
-                    DEFAULT_ACCOUNT, 
-                    this.props.auth.auth
-                );
+                // await V4Keystore.create(
+                //     path.join(accountDirectory, signingKeyString + ".json"),
+                //     password, new Keypair(signingKey)
+                // );
+                
+                // await database.account.set(
+                //     DEFAULT_ACCOUNT, 
+                //     this.props.auth.auth
+                // );
 
             }
             this.props.storeAddValidator(false);
             this.props.history.replace(Routes.DASHBOARD_ROUTE);
-
-        } else {
+        } 
+        /**
+         * In case of registration
+         */
+        else {
             const {register, storeSigningKey, history} = this.props;
 
             const signingKey = Eth2HDWallet.getKeypair(register.signingMnemonic).privateKey.toHexString();
