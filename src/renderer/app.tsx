9059212c6d46f364947e23355fedb63facdc45a6
irubido <ivan.rubido@gmail.com>
2020-01-28 16:53:51 +0100

    draft

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -2,8 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {AppContainer} from "react-hot-loader";
-
-import Application from "./containers/Application";
+import {Application} from "./containers/Application";
 import store from "./store";
 import "./style/index.scss";
 import {ConfirmModal} from "./components/ConfirmModal/ConfirmModal";
@@ -13,32 +12,23 @@ const mainElement = document.createElement("div");
 document.body.appendChild(mainElement);
 
 // Render components
-const render = (Component: () => JSX.Element): void => {
+const render = (Component: any): void => {
+    
+    
     
 
-
-    const handleBeforeQuit = (): boolean => {
-        const x = store.getState();
-        if(x){
-            console.log(x.beforeQuit.beforeQuit);
-            
-            return x.beforeQuit.beforeQuit
-        }else{
-            return false
-        }
-    }
-    
-    
     ReactDOM.render(
         <AppContainer>
             <Provider store={store}>
+
                 <Component />
-                <ConfirmModal
+                {/* <ConfirmModal
                     showModal={handleBeforeQuit()}
                     question="Are you sure?"
                     onOKClick={()=>{}}
                     onCancelClick={()=>{}}
-                />
+                /> */}
+
             </Provider>
         </AppContainer>,
         mainElement
