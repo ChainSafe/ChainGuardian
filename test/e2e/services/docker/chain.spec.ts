0828c59f85ba6b884a5a1b489756ac2b0df78424
morrigan <belma.dzi@gmail.com>
2020-02-06 14:08:22 +0100

    Write test for catching logs

--- test/e2e/services/docker/chain.spec.ts
+++ test/e2e/services/docker/chain.spec.ts
@@ -1,11 +1,12 @@
-import {BeaconChain} from "../../../../src/renderer/services/docker/chain";
+import {BeaconChain} from '../../../../src/renderer/services/docker/chain';
 import { runCmdAsync } from '../../../../src/renderer/services/utils/cmd-utils';
+import { assert } from 'chai';
 
 // run docker e2e test suit if env variable set
-if (process.env["RUN_DOCKER_TESTS"] === "yes")
-    describe("docker container e2e tests", tests);
+if (process.env['RUN_DOCKER_TESTS'] === 'yes')
+    describe('docker container e2e tests', tests);
 else
-    describe.skip("skipping docker container e2e tests", tests);
+    describe.skip('skipping docker container e2e tests', tests);
 
 function tests(): void {
     let beaconChain: BeaconChain;
@@ -17,7 +18,7 @@ function tests(): void {
             if (isRunning) {
                 await beaconChain.stop();
             }
-            await runCmdAsync("docker rm Prysm-beacon-node").catch();
+            await runCmdAsync('docker rm Prysm-beacon-node').catch();
         }
     }, 20000);
 
@@ -26,14 +27,38 @@ function tests(): void {
      * 1) run docker instance of prysm beacon chain
      * 2) check if instance is running using cmdRun
      */
-    it("should execute test case: run-check", async done => {
+    it('should execute test case: run-check', async done => {
         if (await BeaconChain.isDockerInstalled()) {
             beaconChain = BeaconChain.startPrysmBeaconChain();
             // wait for docker instance to start
-            await beaconChain.run(true);
+            await beaconChain.run();
 
-            while (!(await beaconChain.isRunning())) { /* */ }
+            while (!(await beaconChain.isRunning())) { /* */}
             done();
+        }
+    }, 10000);
+
+    /**
+     * TEST CASE: start Prysm chain and fetch logs
+     * 1) run docker instance of prysm beacon chain
+     * 2) check if instance is running using cmdRun
+     */
+    it('should read logs (from stderr)', async done => {
+        if (await BeaconChain.isDockerInstalled()) {
+            beaconChain = BeaconChain.startPrysmBeaconChain();
+            // wait for docker instance to start
+            await beaconChain.run();
+
+            while (!(await beaconChain.isRunning())) {}
+            const logs = beaconChain.getLogs();
+            if (!logs) {
+                return assert(false, 'Logs not found');
+            }
+
+            logs.stderr.on('data', function(chunk: any) {
+                assert(chunk);
+                done();
+            });
         }
     }, 10000);
 }
