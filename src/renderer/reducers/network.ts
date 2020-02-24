14ed3730695bada4a31966fd79bceccd7248795d
morrigan <belma.dzi@gmail.com>
2020-02-24 16:06:42 +0100

    no message

--- src/renderer/reducers/network.ts
+++ src/renderer/reducers/network.ts
@@ -1,21 +1,28 @@
 import {NetworkActionTypes} from "../constants/action-types";
-import {ISaveSelectedNetworkAction} from "../actions/network";
+import { IBeaconNodePayload, ILoadBeaconNodeAction, ISaveSelectedNetworkAction } from '../actions/network';
+import { Action } from 'redux';
 
 export interface INetworkState {
     selected?: string;
+    beaconNodes: IBeaconNodePayload[];
 }
 
 const initialState: INetworkState = {
     selected: undefined,
+    beaconNodes: [],
 };
 
 export const networkReducer = (
     state = initialState,
-    action: ISaveSelectedNetworkAction): INetworkState => {
+    action: Action<NetworkActionTypes>): INetworkState => {
 
     switch (action.type) {
         case NetworkActionTypes.SELECT_NETWORK:
-            return {...state, selected: action.payload};
+            return {...state, selected: (action as ISaveSelectedNetworkAction).payload};
+        case NetworkActionTypes.LOAD_BEACON_NODE:
+            const nodes = state.beaconNodes;
+            nodes.push((action as ILoadBeaconNodeAction).payload);
+            return {...state, beaconNodes: nodes};
         default:
             return state;
     }
