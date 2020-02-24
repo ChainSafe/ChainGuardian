72e2aac29ada1b549e565ac4d596e543c8958ace
Marin Petrunić <marin.petrunic@gmail.com>
2020-02-24 09:16:38 +0100

    renamed util files

---
+++ src/renderer/services/utils/bytes.ts
@@ -0,0 +1,12 @@
+export function base64Decode(data: string): Buffer {
+    return Buffer.from(data, "base64");
+}
+export function base64Encode(data: Buffer): string {
+    return data.toString("base64");
+}
+export function fromHex(data: string): Buffer {
+    return Buffer.from(data, "hex");
+}
+export function toHex(data: Buffer): string {
+    return data.toString("hex");
+}
