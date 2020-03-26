a789bcf16e34f193b4fb4a72c975dd553a1f8ca9
Marin Petrunić <marin.petrunic@gmail.com>
2020-03-26 11:53:26 +0100

    update configuration for deposit submission

--- eth1/init.sh
+++ eth1/init.sh
@@ -2,8 +2,10 @@
 rm -rf /opt/eth1datadir
 if [ ! -d /root/.ethereum/keystore ]; then
     echo "/root/.ethereum/keystore not found, running 'geth init'..."
-    geth init --datadir /opt/eth1datadir /tmp/genesis.json
-    geth account import /tmp/signer.key --datadir /opt/eth1datadir --password "/tmp/passwd.txt"
+    geth --nousb init --datadir /opt/eth1datadir /tmp/genesis.json
+    geth --nousb account import /tmp/signer.key --datadir /opt/eth1datadir --password "/tmp/passwd.txt"
+    echo "Display account list"
+    geth --nousb account list --datadir /opt/eth1datadir
     echo "...done!"
 fi
 
