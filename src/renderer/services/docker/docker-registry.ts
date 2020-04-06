ac9447962df2fe63c4c06cc48b4ab8d9bd843c14
morrigan <belma.dzi@gmail.com>
2020-04-06 16:58:15 +0200

    Use event to stop docker containers

--- src/renderer/services/docker/docker-registry.ts
+++ src/renderer/services/docker/docker-registry.ts
@@ -1,3 +1,5 @@
+import {ipcRenderer} from "electron";
+
 import {SupportedNetworks} from "../eth2/supportedNetworks";
 import {Container} from "./container";
 
@@ -17,7 +19,17 @@ class DockerRegistryClass {
     public getContainer(network: SupportedNetworks): Container|null {
         return this.DockerRegistry[network];
     }
+
+    public async stopAll(): Promise<void> {
+        await Promise.all(
+            Object.values(this.DockerRegistry).map(container => container.stop())
+        );
+    }
 }
 
 export const DockerRegistry = new DockerRegistryClass();
 Object.freeze(DockerRegistry);
+
+ipcRenderer.on('stop-docker', async() => {
+    await DockerRegistry.stopAll();
+});
