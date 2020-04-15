77e2e0a074439ba4b0d9638737553846f7f0c23b
morrigan <belma.dzi@gmail.com>
2020-04-15 12:46:22 +0200

    Refactor some functions in login

--- src/renderer/containers/Login/LoginContainer.tsx
+++ src/renderer/containers/Login/LoginContainer.tsx
@@ -32,7 +32,7 @@ IOwnProps & IInjectedProps & Pick<IRootState, "auth">, IState> {
     public state: IState = {
         input: ""
     };
-    
+
     public render(): ReactElement {
         return (
             <Background>
@@ -40,23 +40,30 @@ IOwnProps & IInjectedProps & Pick<IRootState, "auth">, IState> {
                     <h1>Welcome!</h1>
                     <p>Please enter your password or set up an account to get started.</p>
                     <div className="input-container">
-                        <InputForm 
+                        <InputForm
                             inputId="inputPassword"
-                            focused onChange={this.handleChange} 
-                            inputValue={this.state.input} 
+                            focused
+                            onChange={this.handleChange}
+                            inputValue={this.state.input}
                             placeholder="Enter password"
                             type="password"
                             onSubmit={(e): void => {e.preventDefault();}}
                         />
                         <ButtonSecondary
                             buttonId="go"
-                            onClick={(): void => {this.handleSubmit();}}
-                        >GO</ButtonSecondary>
+                            onClick={this.handleSubmit}
+                        >
+                            GO
+                        </ButtonSecondary>
                     </div>
                     <h5 className="input-or">OR</h5>
-                    <Link to={Routes.ONBOARD_ROUTE_EVALUATE(OnBoardingRoutes.SIGNING)}>
-                        <ButtonPrimary buttonId="register">REGISTER</ButtonPrimary>
-                    </Link>
+
+                    <ButtonPrimary
+                        buttonId="register"
+                        onClick={this.handleRegisterClick}
+                    >
+                        REGISTER
+                    </ButtonPrimary>
                 </Modal>
             </Background>
         );
@@ -65,40 +72,39 @@ IOwnProps & IInjectedProps & Pick<IRootState, "auth">, IState> {
     private handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
         this.setState({input: e.currentTarget.value});
     };
-    
+
     private handleSubmit = async (): Promise<void> => {
         const account = await database.account.get(DEFAULT_ACCOUNT);
-        if(account!==null) {
+        if (account !== null) {
             const isCorrectValue = await account.isCorrectPassword(this.state.input);
-            if(isCorrectValue){
+            if (isCorrectValue) {
                 await account.unlock(this.state.input);
                 this.props.storeAuth(account);
                 this.props.history.push(Routes.DASHBOARD_ROUTE);
             } else {
-                this.props.notification({
-                    source: this.props.history.location.pathname,
-                    isVisible: true,
-                    title: "Incorrect password",
-                    content: "Try again",
-                    horizontalPosition: Horizontal.CENTER,
-                    verticalPosition: Vertical.TOP,
-                    level: Level.ERROR,
-                    expireTime: 3
-                });
+                this.displayNotification("Incorrect password", "Try again");
             }
         } else {
-            this.props.notification({
-                source: this.props.history.location.pathname,
-                isVisible: true,
-                title: "Account does not exist",
-                content: "Please register",
-                horizontalPosition: Horizontal.CENTER,
-                verticalPosition: Vertical.TOP,
-                level: Level.ERROR,
-                expireTime: 3
-            });
+            this.displayNotification("Account does not exist", "Please register");
         }
     };
+
+    private handleRegisterClick = (): void => {
+        this.props.history.push(Routes.ONBOARD_ROUTE_EVALUATE(OnBoardingRoutes.SIGNING));
+    };
+
+    private displayNotification = (title: string, content: string): void => {
+        this.props.notification({
+            source: this.props.history.location.pathname,
+            isVisible: true,
+            title,
+            content,
+            horizontalPosition: Horizontal.CENTER,
+            verticalPosition: Vertical.TOP,
+            level: Level.ERROR,
+            expireTime: 3
+        });
+    }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch): IInjectedProps =>
