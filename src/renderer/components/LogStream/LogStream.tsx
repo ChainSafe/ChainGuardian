d76b3a60a9e6a0ea9a64e60b7252d24fc9c0918d
morrigan <belma.dzi@gmail.com>
2020-04-21 15:48:22 +0200

    Change LogStream to user node.js stream

--- src/renderer/components/LogStream/LogStream.tsx
+++ src/renderer/components/LogStream/LogStream.tsx
@@ -1,28 +1,29 @@
 import * as React from "react";
 import {useState, useEffect} from "react";
+import {Readable} from "stream";
 
 export interface ILogStreamProps {
-    stream: ReadableStream;
+    stream?: Readable;
 }
 
 export const LogStream: React.FunctionComponent<ILogStreamProps> = (props: ILogStreamProps) => {
     const [logs, setLogs] = useState<string[]>([]);
     
-    useEffect(()=>{
-        const reader = props.stream.getReader();
-        reader.read().then(({value}: {value: string})=>{
-            setLogs(logs.concat([value]));
-            reader.releaseLock();
+    useEffect(()=> {
+        props.stream && props.stream.on('data', (chunk: Buffer)=>{
+            setLogs(logs.concat([chunk.toString()]));
         });
     },[logs]);
 
     return(
         <React.Fragment>
+            {logs.length === 0 ? <div className="log-data">There are no logs.</div> : null}
+
             {logs.map((logEntry: string) =>{
                 return(
                     <div key={logEntry} className="log-data">{logEntry}</div>
                 );
             })}
-        </React.Fragment> 
+        </React.Fragment>
     );
 };
