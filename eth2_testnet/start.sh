e2ce3684e2398a7fb39263ca8d14738f73d3fa7a
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-11 10:29:05 +0200

    renaming testnet directories

--- eth2_testnet/start.sh
+++ eth2_testnet/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -rf /opt/cg_testnet/genesis.ssz
+rm -rf /opt/eth2_testnet/genesis.ssz
 rm -rf ~/.lighthouse/beacon
 rm -rf ~/.lighthouse/validators
 /opt/wait-for.sh eth1-node:8545
 sleep 10
-lcli --spec minimal interop-genesis -d /opt/cg_testnet 16
+lcli --spec minimal interop-genesis -d /opt/eth2_testnet 16
 lighthouse -s minimal account validator new \
-  --testnet-dir /opt/cg_testnet \
+  --testnet-dir /opt/eth2_testnet \
   --send-deposits \
   --account-index 0 \
   --eth1-endpoint http://eth1-node:8545 \
@@ -15,4 +15,4 @@ lighthouse -s minimal account validator new \
   --deposit-value 32000000000 \
   insecure \
   0 16
-lighthouse -s minimal bn --eth1 --eth1-endpoint http://eth1-node:8545 --http --http-address 0.0.0.0 --maxpeers 0 --testnet-dir /opt/cg_testnet
+lighthouse -s minimal bn --eth1 --eth1-endpoint http://eth1-node:8545 --http --http-address 0.0.0.0 --maxpeers 0 --testnet-dir /opt/eth2_testnet
