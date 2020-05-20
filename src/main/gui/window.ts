aeaa76635f0dff1ed75e3eecd027966b66172cbb
morrigan <belma.dzi@gmail.com>
2020-05-20 11:07:38 +0200

    Set websecurity true as default

--- src/main/gui/window.ts
+++ src/main/gui/window.ts
@@ -19,7 +19,7 @@ export async function createWindow(): Promise<void> {
     );
     // eslint-disable-next-line require-atomic-updates
     win = new BrowserWindow({
-        webPreferences: {nodeIntegration: true, webSecurity: false},
+        webPreferences: {nodeIntegration: true},
         backgroundColor: "#052437",
         show: false,
         icon: iconPath,
