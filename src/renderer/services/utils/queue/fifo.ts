886060d685ff05136ef9d45c15664c027cb01098
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-28 17:49:47 +0200

    add fifo queue

---
+++ src/renderer/services/utils/queue/fifo.ts
@@ -0,0 +1,26 @@
+import {EventEmitter} from "events";
+
+export class FifoQueue<T> extends EventEmitter {
+
+    private array: T[] = [];
+
+    private readonly max: number;
+
+    public constructor(maxElements: number) {
+        super();
+        this.max = maxElements;
+    }
+
+    public push(item: T): void {
+        this.array.push(item);
+        if(this.array.length > this.max) {
+            this.array.shift();
+        }
+        this.emit("data", item);
+    }
+
+    public getAll(): T[] {
+        return this.array.slice();
+    }
+
+}
