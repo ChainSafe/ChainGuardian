53951172150099398170163639f540e8fa4bed31
morrigan <belma.dzi@gmail.com>
2020-06-09 17:32:39 +0200

    Add notification for errors

--- src/renderer/containers/BeaconNodes/BeaconNodeButtons.tsx
+++ src/renderer/containers/BeaconNodes/BeaconNodeButtons.tsx
@@ -1,30 +1,59 @@
 import React from "react";
 import {useDispatch} from "react-redux";
+import logger from "electron-log";
+import {useHistory} from "react-router";
+import {storeNotificationAction} from "../../actions";
 import {ButtonDestructive, ButtonInverted, ButtonPrimary} from "../../components/Button/ButtonStandard";
 import {DockerRegistry} from "../../services/docker/docker-registry";
 
 interface IBeaconNodeButtonsProps {
-    updateNodeStatus(url: string, status: boolean): void;
     image: string;
     url: string;
     isRunning: boolean;
+    updateNodeStatus(url: string, status: boolean): void;
 }
 
 export const BeaconNodeButtons: React.FunctionComponent<IBeaconNodeButtonsProps> = (props: IBeaconNodeButtonsProps) => {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const onStopClick = async(image: string, url: string): Promise<void> => {
-        await (DockerRegistry.getContainer(image)!).stop();
-        props.updateNodeStatus(url, false);
+        try {
+            await (DockerRegistry.getContainer(image)!).stop();
+            props.updateNodeStatus(url, false);
+        } catch (e) {
+            logger.error(e);
+            dispatch(storeNotificationAction({
+                source: history.location.pathname,
+                title: "Error while trying to stop beacon node container"
+            }));
+        }
     };
 
     const onStartClick = async(image: string, url: string): Promise<void> => {
-        await (DockerRegistry.getContainer(image)!).startStoppedContainer();
-        props.updateNodeStatus(url, true);
+        try {
+            await (DockerRegistry.getContainer(image)!).startStoppedContainer();
+            props.updateNodeStatus(url, true);
+        } catch (e) {
+            logger.error(e);
+            dispatch(storeNotificationAction({
+                source: history.location.pathname,
+                title: "Error while trying to start beacon node container"
+            }));
+        }
     };
 
     const onRemoveClick = async(image: string): Promise<void> => {
-        await (DockerRegistry.getContainer(image)!).remove();
+        try {
+            await (DockerRegistry.getContainer(image)!).remove();
+            // TODO: Dispatch?
+        } catch (e) {
+            logger.error(e);
+            dispatch(storeNotificationAction({
+                source: history.location.pathname,
+                title: "Error while trying to remove beacon node container"
+            }));
+        }
     };
 
     const {image, url, isRunning} = props;
@@ -43,5 +72,5 @@ export const BeaconNodeButtons: React.FunctionComponent<IBeaconNodeButtonsProps>
             }
             <ButtonDestructive onClick={(): Promise<void> => onRemoveClick(image)}>Remove</ButtonDestructive>
         </div>
-    )
+    );
 };
