9c3c0c9b8bb1c7ffdbf6b33c991c28a0b46059a4
morrigan <belma.dzi@gmail.com>
2020-06-09 15:59:20 +0200

    Manage docker containers with buttons

--- src/renderer/containers/BeaconNodes/BeaconNodes.tsx
+++ src/renderer/containers/BeaconNodes/BeaconNodes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {useHistory} from "react-router";
 import {Background} from "../../components/Background/Background";
@@ -7,13 +7,18 @@ import {ButtonDestructive, ButtonInverted, ButtonPrimary} from "../../components
 import {NodeCard} from "../../components/Cards/NodeCard";
 import {BeaconNode} from "../../models/beaconNode";
 import {IRootState} from "../../reducers";
+import {DockerRegistry} from "../../services/docker/docker-registry";
 import {truncatePublicKey} from "../../services/utils/formatting";
 
-interface extendedBeaconNode extends BeaconNode {
+interface ExtendedBeaconNode extends BeaconNode {
     validators: string[]
 }
-type beaconNodes = {
-    [url: string]: extendedBeaconNode;
+type BeaconNodes = {
+    [url: string]: ExtendedBeaconNode;
+}
+
+type RunningBeaconNodes = {
+    [url: string]: boolean,
 }
 
 export const BeaconNodesContainer: React.FunctionComponent = () => {
@@ -21,17 +26,76 @@ export const BeaconNodesContainer: React.FunctionComponent = () => {
     const validatorBeaconNodes = useSelector((state: IRootState) => state.network.validatorBeaconNodes);
     const validators = useSelector((state: IRootState) => state.validators);
 
-    const allNodes: beaconNodes = {};
-    for (let [validatorAddress, beaconNodes] of Object.entries(validatorBeaconNodes)) {
-        beaconNodes.map(node => {
-            allNodes[node.url] = {
-                ...node,
-                validators: allNodes[node.url] ? allNodes[node.url].validators : [],
-            };
-            allNodes[node.url].validators.push(validatorAddress)
-        });
-    }
+    const [runningBeaconNodes, setRunningBeaconNodes] = useState<RunningBeaconNodes>({});
+    const [allNodes, setAllNodes] = useState<BeaconNodes>({});
     const nodeList = Object.keys(allNodes);
+
+    useEffect(() => {
+        for (let [validatorAddress, beaconNodes] of Object.entries(validatorBeaconNodes)) {
+            beaconNodes.map(node => {
+                const validators = allNodes[node.url] ? allNodes[node.url].validators : [];
+                validators.push(validatorAddress);
+                setAllNodes({
+                    ...allNodes,
+                    [node.url]: {
+                        ...node,
+                        validators,
+                    }
+                });
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        nodeList.map(async (url) => {
+            if (allNodes[url].localDockerId) {
+                const container = DockerRegistry.getContainer(allNodes[url].localDockerId);
+                if (container) {
+                    const isRunning = await container.isRunning();
+                    setRunningBeaconNodes({
+                        ...runningBeaconNodes,
+                        [url]: isRunning,
+                    })
+                }
+            }
+        });
+    }, [allNodes]);
+
+    const onStopClick = async(image: string, url: string): Promise<void> => {
+        await (DockerRegistry.getContainer(image)!).stop();
+        setRunningBeaconNodes({
+            ...runningBeaconNodes,
+            [url]: false,
+        })
+    };
+
+    const onStartClick = async(image: string, url: string): Promise<void> => {
+        await (DockerRegistry.getContainer(image)!).startStoppedContainer();
+        setRunningBeaconNodes({
+            ...runningBeaconNodes,
+            [url]: true,
+        })
+    };
+
+    const onRemoveClick = async(image: string): Promise<void> => {
+        await (DockerRegistry.getContainer(image)!).remove();
+    };
+
+    const renderButtons = (image: string, url: string): React.ReactElement => (
+        <div className="row buttons">
+            {
+                image ?
+                    <>
+                        {runningBeaconNodes[url] ?
+                            <ButtonInverted onClick={(): Promise<void> => onStopClick(image, url)}>Stop</ButtonInverted>
+                            :
+                            <ButtonPrimary onClick={(): Promise<void> => onStartClick(image, url)}>Start</ButtonPrimary>
+                        }
+                    </> : null
+            }
+            <ButtonDestructive onClick={(): Promise<void> => onRemoveClick(image)}>Remove</ButtonDestructive>
+        </div>
+    );
 
     return (
         <Background scrollable={true}>
@@ -45,43 +109,36 @@ export const BeaconNodesContainer: React.FunctionComponent = () => {
                     <div className="flex-column">
                         {nodeList.length === 0 ? <h3>No beacon nodes found.</h3> : null}
 
-                        {nodeList.map((url) => (
-                            <div className="row box node-container">
-                                <NodeCard
-                                    key={url}
-                                    onClick={() => (): void => {}}
-                                    title={allNodes[url].localDockerId ? "Local Docker container" : "Remote Beacon node"}
-                                    url={url}
-                                    isSyncing={allNodes[url].isSyncing}
-                                    value={allNodes[url].currentSlot || "N/A"}
-                                />
+                        {nodeList.map((url) => {
+                            const node = allNodes[url];
+                            return (
+                                <div className="row box node-container" key={url}>
+                                    <NodeCard
+                                        onClick={() => (): void => {}}
+                                        title={node.localDockerId ? "Local Docker container" : "Remote Beacon node"}
+                                        url={url}
+                                        isSyncing={node.isSyncing}
+                                        value={node.currentSlot || "N/A"}
+                                    />
 
-                                <div className="flex-column stretch space-between">
-                                    <div className="flex-column">
-                                        <h5>Connected validators:</h5>
+                                    <div className="flex-column stretch space-between">
+                                        <div className="flex-column">
+                                            <h5>Connected validators:</h5>
 
-                                        {allNodes[url] && allNodes[url].validators.map(validatorAddress => (
-                                            <div className="flex-column" key={validatorAddress}>
-                                                <p><b>{validators[validatorAddress].name} </b>
-                                                     - {truncatePublicKey(validatorAddress)}
-                                                </p>
-                                            </div>
-                                        ))}
-                                    </div>
+                                            {node && node.validators.map(validatorAddress => (
+                                                <div className="flex-column" key={validatorAddress}>
+                                                    <p><b>{validators[validatorAddress].name} </b>
+                                                        - {truncatePublicKey(validatorAddress)}
+                                                    </p>
+                                                </div>
+                                            ))}
+                                        </div>
 
-                                    <div className="row buttons">
-                                        {
-                                            allNodes[url].localDockerId ?
-                                            <>
-                                                <ButtonPrimary>Start</ButtonPrimary>
-                                                <ButtonInverted>Stop</ButtonInverted>
-                                            </> : null
-                                        }
-                                        <ButtonDestructive>Remove</ButtonDestructive>
+                                        {renderButtons(node.localDockerId, url)}
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            )
+                        })}
                     </div>
                 </div>
             </div>
