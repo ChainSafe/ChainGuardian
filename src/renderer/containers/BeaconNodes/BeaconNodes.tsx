0a707592411dc82993286dd445da290fbdd52e35
morrigan <belma.dzi@gmail.com>
2020-06-15 14:31:45 +0200

    Add loading

--- src/renderer/containers/BeaconNodes/BeaconNodes.tsx
+++ src/renderer/containers/BeaconNodes/BeaconNodes.tsx
@@ -4,6 +4,7 @@ import {useHistory} from "react-router";
 import {Background} from "../../components/Background/Background";
 import {BackButton} from "../../components/Button/ButtonAction";
 import {NodeCard} from "../../components/Cards/NodeCard";
+import {Loading} from "../../components/Loading/Loading";
 import {BeaconNode} from "../../models/beaconNode";
 import {IRootState} from "../../reducers";
 import {DockerRegistry} from "../../services/docker/docker-registry";
@@ -26,6 +27,7 @@ export const BeaconNodesContainer: React.FunctionComponent = () => {
     const validatorBeaconNodes = useSelector((state: IRootState) => state.network.validatorBeaconNodes);
     const validators = useSelector((state: IRootState) => state.validators);
 
+    const [loading, setLoading] = useState<boolean>(true);
     const [runningBeaconNodes, setRunningBeaconNodes] = useState<RunningBeaconNodes>({});
     const [allNodes, setAllNodes] = useState<BeaconNodes>({});
     const nodeList = Object.keys(allNodes);
@@ -44,6 +46,7 @@ export const BeaconNodesContainer: React.FunctionComponent = () => {
             });
             setAllNodes(beaconNodesList);
         }
+        setLoading(false);
     }, [validatorBeaconNodes]);
 
     useEffect(() => {
@@ -70,56 +73,60 @@ export const BeaconNodesContainer: React.FunctionComponent = () => {
     };
 
     return (
-        <Background scrollable={true}>
-            <div className="flex-column validator-container beacon-nodes-container">
-                <div className="row">
-                    <BackButton onClick={(): void => history.goBack()} />
-                    <h2>Beacon nodes management</h2>
-                </div>
+        <>
+            <Background scrollable={true}>
+                <div className="flex-column validator-container beacon-nodes-container">
+                    <div className="row">
+                        <BackButton onClick={(): void => history.goBack()} />
+                        <h2>Beacon nodes management</h2>
+                    </div>
 
-                <div className="validator-nodes">
-                    <div className="flex-column">
-                        {nodeList.length === 0 ? <h3>No beacon nodes found.</h3> : null}
+                    <div className="validator-nodes">
+                        <div className="flex-column">
+                            {nodeList.length === 0 ? <h3>No beacon nodes found.</h3> : null}
 
-                        {nodeList.map((url) => {
-                            const node = allNodes[url];
-                            return (
-                                <div className="row box node-container" key={url}>
-                                    <NodeCard
-                                        onClick={() => (): void => {}}
-                                        title={node.localDockerId ? "Local Docker container" : "Remote Beacon node"}
-                                        url={url}
-                                        isSyncing={node.isSyncing}
-                                        value={node.currentSlot || "N/A"}
-                                    />
-
-                                    <div className="flex-column stretch space-between">
-                                        <div className="flex-column">
-                                            <h5>Connected validators:</h5>
-
-                                            {node && node.validators.map(validatorAddress => (
-                                                <div className="flex-column" key={validatorAddress}>
-                                                    <p><b>{validators[validatorAddress].name} </b>
-                                                        - {truncatePublicKey(validatorAddress)}
-                                                    </p>
-                                                </div>
-                                            ))}
-                                        </div>
-
-                                        <BeaconNodeButtons
-                                            updateNodeStatus={onUpdateNodeStatus}
-                                            image={node.localDockerId}
+                            {nodeList.map((url) => {
+                                const node = allNodes[url];
+                                return (
+                                    <div className="row box node-container" key={url}>
+                                        <NodeCard
+                                            onClick={() => (): void => {}}
+                                            title={node.localDockerId ? "Local Docker container" : "Remote Beacon node"}
                                             url={url}
-                                            isRunning={runningBeaconNodes[url]}
-                                            validators={node.validators}
+                                            isSyncing={node.isSyncing}
+                                            value={node.currentSlot || "N/A"}
                                         />
+
+                                        <div className="flex-column stretch space-between">
+                                            <div className="flex-column">
+                                                <h5>Connected validators:</h5>
+
+                                                {node && node.validators.map(validatorAddress => (
+                                                    <div className="flex-column" key={validatorAddress}>
+                                                        <p><b>{validators[validatorAddress].name} </b>
+                                                            - {truncatePublicKey(validatorAddress)}
+                                                        </p>
+                                                    </div>
+                                                ))}
+                                            </div>
+
+                                            <BeaconNodeButtons
+                                                updateNodeStatus={onUpdateNodeStatus}
+                                                image={node.localDockerId}
+                                                url={url}
+                                                isRunning={runningBeaconNodes[url]}
+                                                validators={node.validators}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Background>
+            </Background>
+
+            <Loading visible={loading} title="Loading" />
+        </>
     );
 };
