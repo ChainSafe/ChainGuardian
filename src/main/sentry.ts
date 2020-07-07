ac04f0d540395ef67cbb02b029eadc3461619037
morrigan <belma.dzi@gmail.com>
2020-07-07 16:27:57 +0200

    Skip sentry Big Int error report

--- src/main/sentry.ts
+++ src/main/sentry.ts
@@ -1,3 +1,4 @@
+import {Event} from "@sentry/electron";
 /* eslint-disable @typescript-eslint/no-require-imports */
 export function initSentry(): void {
     if (process.env.NODE_ENV === "production") {
@@ -9,6 +10,18 @@ export function initSentry(): void {
             ? require("electron").app.getVersion()
             : require("electron").remote.app.getVersion());
 
-        init({dsn: process.env.SENTRY_DSN, release: version});
+        init({
+            dsn: process.env.SENTRY_DSN,
+            release: version,
+            beforeSend: (event: Event) => {
+                if (event.exception && event.exception.values.length > 0 &&
+                    event.exception.values[0].value.includes("Do not know how to serialize a BigInt"))
+                {
+                    return null;
+                }
+
+                return event;
+            },
+        });
     }
 }
