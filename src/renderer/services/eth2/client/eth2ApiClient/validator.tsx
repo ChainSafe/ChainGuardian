5b545a23b326a43b6baf09f089106c8de98cfc82
Bernard <bero4net@gmail.com>
2020-12-09 15:57:26 +0100

    implement validator rest methods

--- src/renderer/services/eth2/client/eth2ApiClient/validator.tsx
+++ src/renderer/services/eth2/client/eth2ApiClient/validator.tsx
@@ -4,7 +4,6 @@ import {
     AttestationData,
     AttesterDuty,
     BeaconBlock,
-    BLSPubkey,
     CommitteeIndex,
     Epoch,
     ProposerDuty,
@@ -15,6 +14,8 @@ import {
 } from "@chainsafe/lodestar-types";
 import {HttpClient} from "../../../api";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
+import {Json, toHexString} from "@chainsafe/ssz";
+import querystring from "querystring";
 
 export class Validator implements ICGEth2ValidatorApi {
     private readonly httpClient: HttpClient;
@@ -25,18 +26,29 @@ export class Validator implements ICGEth2ValidatorApi {
     }
 
     public getAggregatedAttestation = async (attestationDataRoot: Root, slot: Slot): Promise<Attestation> => {
-        console.log("getAggregatedAttestation", attestationDataRoot, slot);
-        return undefined as Attestation;
+        const query = querystring.stringify({
+            // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+            attestation_data_root: this.config.types.Root.toJson(attestationDataRoot) as string,
+            slot,
+        });
+        const url = `/eth/v1/validator/aggregate_attestation?${query}`;
+        const responseData = await this.httpClient.get<{data: Json[]}>(url);
+        return this.config.types.Attestation.fromJson(responseData.data, {case: "snake"});
     };
 
     public getAttesterDuties = async (epoch: Epoch, validatorPubKeys: ValidatorIndex[]): Promise<AttesterDuty[]> => {
-        console.log("getAttesterDuties", epoch, validatorPubKeys);
-        return undefined as AttesterDuty[];
+        const url = `/eth/v1/validator/duties/attester/${epoch.toString()}`;
+        const responseData = await this.httpClient.post<string[], {data: Json[]}>(
+            url,
+            validatorPubKeys.map((index) => this.config.types.ValidatorIndex.toJson(index) as string),
+        );
+        return responseData.data.map((value) => this.config.types.AttesterDuty.fromJson(value, {case: "snake"}));
     };
 
-    public getProposerDuties = async (epoch: Epoch, validatorPubKeys: BLSPubkey[]): Promise<ProposerDuty[]> => {
-        console.log("getProposerDuties", epoch, validatorPubKeys);
-        return undefined as ProposerDuty[];
+    public getProposerDuties = async (epoch: Epoch): Promise<ProposerDuty[]> => {
+        const url = `/eth/v1/validator/duties/proposer/${epoch.toString()}`;
+        const responseData = await this.httpClient.get<{data: Json[]}>(url);
+        return responseData.data.map((value) => this.config.types.ProposerDuty.fromJson(value, {case: "snake"}));
     };
 
     public prepareBeaconCommitteeSubnet = async (
@@ -46,27 +58,45 @@ export class Validator implements ICGEth2ValidatorApi {
         slot: Slot,
         isAggregator: boolean,
     ): Promise<void> => {
-        console.log(
-            "prepareBeaconCommitteeSubnet",
-            validatorIndex,
-            committeeIndex,
-            committeesAtSlot,
-            slot,
-            isAggregator,
-        );
+        await this.httpClient.post<Json[], void>("/beacon_committee_subscriptions", [
+            {
+                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+                validator_index: validatorIndex,
+                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+                committee_index: committeeIndex,
+                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+                committees_at_slot: committeesAtSlot,
+                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+                is_aggregator: isAggregator,
+                slot,
+            },
+        ]);
     };
 
     public produceAttestationData = async (index: CommitteeIndex, slot: Slot): Promise<AttestationData> => {
-        console.log("produceAttestationData", index, slot);
-        return undefined as AttestationData;
+        const query = querystring.stringify({
+            // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+            committee_index: index,
+            slot,
+        });
+        const responseData = await this.httpClient.get<{data: Json[]}>(`/eth/v1/validator/attestation_data?${query}`);
+        return this.config.types.AttestationData.fromJson(responseData.data, {case: "snake"});
     };
 
     public produceBlock = async (slot: Slot, randaoReveal: Uint8Array, graffiti: string): Promise<BeaconBlock> => {
-        console.log("produceBlock", slot, randaoReveal, graffiti);
-        return undefined as BeaconBlock;
+        const query = querystring.stringify({
+            // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
+            randao_reveal: toHexString(randaoReveal),
+            graffiti: graffiti,
+        });
+        const responseData = await this.httpClient.get<{data: Json}>(`/eth/v1/validator/blocks/${slot}?${query}`);
+        return this.config.types.BeaconBlock.fromJson(responseData.data, {case: "snake"});
     };
 
     public publishAggregateAndProofs = async (signedAggregateAndProofs: SignedAggregateAndProof[]): Promise<void> => {
-        console.log("publishAggregateAndProofs", signedAggregateAndProofs);
+        await this.httpClient.post<Json[], void>(
+            "/aggregate_and_proofs",
+            signedAggregateAndProofs.map((a) => this.config.types.SignedAggregateAndProof.toJson(a, {case: "snake"})),
+        );
     };
 }
