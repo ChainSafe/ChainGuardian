c1b0b750a3c6d72eac010b88b32c8cdba2181909
Bernard <bero4net@gmail.com>
2020-12-11 09:13:27 +0100

    improve docker config

--- testnet/start-beacon-node.sh
+++ testnet/start-beacon-node.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 exec lighthouse --debug-level $DEBUG_LEVEL beacon_node \
-  --dummy-eth1 --disable-discovery \
-  --http --http-address 0.0.0.0 \
+  --dummy-eth1 --http --http-address 0.0.0.0 \
   --testnet-dir /root/testnet \
-  --datadir /root/beacon \
-  --enr-address 127.0.0.1 --enr-udp-port 9000 --enr-tcp-port 9000
+  --datadir /root/beacon
--- testnet/start-validator-client.sh
+++ testnet/start-validator-client.sh
@@ -3,5 +3,4 @@
 exec lighthouse --debug-level $DEBUG_LEVEL validator \
   --allow-unsynced --init-slashing-protection \
   --testnet-dir /root/testnet \
-  --datadir /root \
-  --beacon-node http://localhost:5052 deprecated use --beacon-node
+  --datadir /root
