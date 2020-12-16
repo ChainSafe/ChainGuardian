69cbc67054da44a32086ae37b4d364ca0afbd267
Bernard <bero4net@gmail.com>
2020-12-16 12:27:46 +0100

    implement second beacon node

--- testnet/lighthouse/start-beacon-node.sh
+++ testnet/lighthouse/start-beacon-node.sh
@@ -2,5 +2,6 @@
 
 exec lighthouse --debug-level $DEBUG_LEVEL beacon_node \
   --dummy-eth1 --http --http-address 0.0.0.0 \
+  --enr-address 127.0.0.1 --enr-udp-port 9000 --enr-tcp-port 9000 \
   --testnet-dir /root/testnet \
   --datadir /root/beacon
