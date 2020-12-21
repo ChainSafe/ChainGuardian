31de1a6b0b84013af49bd0d69d01dcec960b9c65
Bernard <bero4net@gmail.com>
2020-12-21 10:42:50 +0100

    simplified load validator flow

--- src/renderer/ducks/auth/actions.ts
+++ src/renderer/ducks/auth/actions.ts
@@ -1,10 +1,3 @@
-import {DEFAULT_ACCOUNT} from "../../constants/account";
 import {authSlice} from "./slice";
-import {createAction} from "@reduxjs/toolkit";
 
 export const {storeAuth} = authSlice.actions;
-
-export const requireAuthorization = createAction<(account?: string) => {payload: string}>(
-    "auth/requireAuthorization",
-    (account = DEFAULT_ACCOUNT) => ({payload: account}),
-);
