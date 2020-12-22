cd0ca40015e11fc1769afe865764b0e4c73948a6
Bernard <bero4net@gmail.com>
2020-12-22 13:04:05 +0100

    fix tests

--- test/e2e/redux/auth/sagas.spec.ts
+++ test/e2e/redux/auth/sagas.spec.ts
@@ -1,11 +1,11 @@
 import {testSaga} from "redux-saga-test-plan";
 import {authorize} from "../../../../src/renderer/ducks/auth/sagas";
-import {requireAuthorization, storeAuth} from "../../../../src/renderer/ducks/auth/actions";
+import {storeAuth} from "../../../../src/renderer/ducks/auth/actions";
 import {CGAccount} from "../../../../src/renderer/models/account";
 
 describe("auth sagas", () => {
     it("authorize saga without user in DB", () => {
-        testSaga(authorize, requireAuthorization()).next().next(null).isDone();
+        testSaga(authorize).next().next(null).isDone();
     });
 
     it("authorize saga whit user in DB", () => {
@@ -14,6 +14,6 @@ describe("auth sagas", () => {
             directory: "/testdirectory/",
             sendStats: false,
         });
-        testSaga(authorize, requireAuthorization()).next().next(account).put(storeAuth(account)).next().isDone();
+        testSaga(authorize).next().next(account).put(storeAuth(account)).next().isDone();
     });
 });
