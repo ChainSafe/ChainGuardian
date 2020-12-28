a0a2e05a6b3b504793c310830de6d78df0a30fb2
Bernard <bero4net@gmail.com>
2020-12-28 18:51:07 +0100

    implement slot beacon state and refreshing

--- src/renderer/ducks/beacon/sagas.ts
+++ src/renderer/ducks/beacon/sagas.ts
@@ -1,9 +1,22 @@
-import {Action} from "redux";
-import {all, call, put, takeEvery, PutEffect, CallEffect, RaceEffect, TakeEffect, race, take} from "redux-saga/effects";
+import {
+    all,
+    call,
+    put,
+    fork,
+    takeEvery,
+    PutEffect,
+    CallEffect,
+    RaceEffect,
+    TakeEffect,
+    race,
+    take,
+    ChannelTakeEffect,
+    cancel,
+} from "redux-saga/effects";
 import {getNetworkConfig} from "../../services/eth2/networks";
 import {liveProcesses} from "../../services/utils/cmd";
 import {cancelDockerPull, endDockerImagePull, startDockerImagePull} from "../network/actions";
-import {startLocalBeacon, removeBeacon, addBeacon, addBeacons} from "./actions";
+import {startLocalBeacon, removeBeacon, addBeacon, addBeacons, updateSlot} from "./actions";
 import {BeaconChain} from "../../services/docker/chain";
 import {SupportedNetworks} from "../../services/eth2/supportedNetworks";
 import database from "../../services/db/api/database";
@@ -11,6 +24,14 @@ import {Beacons} from "../../models/beacons";
 import {postInit} from "../store";
 import {BeaconStatus} from "./slice";
 import {HttpClient} from "../../services/api";
+import {EventChannel, eventChannel} from "redux-saga";
+import {Action} from "redux";
+import {CgEth2ApiClient} from "../../services/eth2/client/eth2ApiClient";
+import {mainnetConfig} from "@chainsafe/lodestar-config/lib/presets/mainnet";
+import {BeaconEventType, HeadEvent} from "@chainsafe/lodestar-validator/lib/api/interface/events";
+import {AllEffect, CancelEffect, ForkEffect} from "@redux-saga/core/effects";
+import {readBeaconChainNetwork} from "../../services/eth2/client";
+import {INetworkConfig} from "../../services/interfaces";
 
 export function* pullDockerImage(
     network: string,
@@ -64,11 +85,12 @@ function* startLocalBeaconSaga({
     }
 }
 
-function* storeBeacon({payload: {url, docker}}: ReturnType<typeof addBeacon>): Generator<Promise<void>> {
+function* storeBeacon({payload: {url, docker}}: ReturnType<typeof addBeacon>): Generator<Promise<void> | ForkEffect> {
     if (!docker)
         // eslint-disable-next-line no-param-reassign
         docker = {id: "", network: "", chainDataDir: "", eth1Url: "", discoveryPort: "", libp2pPort: "", rpcPort: ""};
     yield database.beacons.upsert({url, docker});
+    yield fork(watchOnHead, url);
 }
 
 function* removeBeaconSaga({payload}: ReturnType<typeof removeBeacon>): Generator<Promise<[boolean, boolean]>> {
@@ -76,9 +98,9 @@ function* removeBeaconSaga({payload}: ReturnType<typeof removeBeacon>): Generato
 }
 
 function* initializeBeaconsFromStore(): Generator<
-    Promise<Beacons> | PutEffect | Promise<void> | Promise<Response[]>,
+    Promise<Beacons> | PutEffect | Promise<void> | Promise<Response[]> | AllEffect<ForkEffect>,
     void,
-    Beacons & (boolean | null)[]
+    Beacons & ({syncing: boolean; slot: number} | null)[]
 > {
     const store = yield database.beacons.get();
     if (store !== null) {
@@ -90,26 +112,71 @@ function* initializeBeaconsFromStore(): Generator<
             beacons.map(({url}) =>
                 new HttpClient(url)
                     // eslint-disable-next-line camelcase
-                    .get<{data: {is_syncing: boolean}}>("/eth/v1/node/syncing")
-                    .then((response) => response.data.is_syncing)
+                    .get<{data: {is_syncing: boolean; head_slot: number}}>("/eth/v1/node/syncing")
+                    .then((response) => ({syncing: response.data.is_syncing, slot: response.data.head_slot}))
                     .catch(() => null),
             ),
         );
+
+        yield all(beacons.map(({url}) => fork(watchOnHead, url)));
 
         yield put(
             addBeacons(
                 beacons.map(({url, docker}, index) => ({
                     url,
                     docker: docker.id !== "" ? docker : undefined,
+                    slot: stats[index]?.slot || 0,
                     status:
-                        stats[index] !== null
-                            ? stats[index]
+                        stats[index].syncing !== null
+                            ? stats[index].syncing
                                 ? BeaconStatus.syncing
                                 : BeaconStatus.active
                             : BeaconStatus.offline,
                 })),
             ),
         );
+    }
+}
+
+export function* watchOnHead(
+    url: string,
+): Generator<
+    | EventChannel<HeadEvent>
+    | ChannelTakeEffect<HeadEvent>
+    | PutEffect
+    | CancelEffect
+    | RaceEffect<ChannelTakeEffect<HeadEvent> | TakeEffect>
+    | Promise<INetworkConfig | null>,
+    void,
+    EventChannel<HeadEvent> & HeadEvent & [HeadEvent, ReturnType<typeof removeBeacon>] & (INetworkConfig | null)
+> {
+    const config = yield readBeaconChainNetwork(url);
+    const client = new CgEth2ApiClient(config?.eth2Config || mainnetConfig, url);
+    const emitter = client.events.getEventStream([BeaconEventType.HEAD]);
+    const event = yield eventChannel<HeadEvent>((emit) => {
+        (async (): Promise<void> => {
+            for await (const event of emitter) {
+                emit(event as HeadEvent);
+            }
+        })();
+        return (): void => {
+            emitter.stop();
+        };
+    });
+
+    while (true) {
+        try {
+            const [payload, cancelAction] = yield race([take(event), take(removeBeacon)]);
+            if (cancelAction) {
+                if (cancelAction.payload === url) {
+                    yield cancel();
+                }
+                continue;
+            }
+            yield put(updateSlot(payload.message.slot, url));
+        } catch (err) {
+            console.error("Head event error:", err.message);
+        }
     }
 }
 
