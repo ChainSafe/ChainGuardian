174165584892fb225e817f4050bba220929849d7
Marin Petrunić <marin.petrunic@gmail.com>
2021-01-07 10:56:02 +0100

    fix env variables and sentry source upload

--- webpack.base.prod.config.js
+++ webpack.base.prod.config.js
@@ -1,15 +1,35 @@
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 const packageJson = require("./package.json");
+const webpack = require("webpack");
+
+const plugins = [
+    new webpack.DefinePlugin({
+        "process.env.SENTRY_DSN": JSON.stringify(process.env.SENTRY_DSN || ""),
+        "process.env.SENTRY_ORG": JSON.stringify(process.env.SENTRY_ORG || "nodefactory"),
+        "process.env.SENTRY_PROJECT": JSON.stringify(process.env.SENTRY_PROJECT || "chainguardian"),
+        "process.env.SENTRY_AUTH_TOKEN": JSON.stringify(process.env.SENTRY_AUTH_TOKEN || ""),
+    }),
+];
+
+if(process.env.SENTRY_AUTH_TOKEN) {
+    // Sentry CLI used for uploading releases
+    plugins.push(new SentryWebpackPlugin({
+
+        // sentry-cli configuration
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: process.env.SENTRY_ORG,
+        project: process.env.SENTRY_PROJECT,
+
+        errorHandler: () => null,
+        include: ["dist", "src"],
+        release: packageJson.version,
+    }));
+} else {
+    console.warn("Sentry source map not uploaded");
+}
+
 
 module.exports = {
     devtool: "source-map",
-    plugins: [
-        // Sentry CLI used for uploading releases
-        new SentryWebpackPlugin({
-            errorHandler: () => null,
-            include: ".",
-            ignore: ["node_modules", "webpack.*.config.js", ".eslintrc.js", "/coverage", "/mocks"],
-            release: packageJson.version,
-        }),
-    ]
+    plugins
 };
