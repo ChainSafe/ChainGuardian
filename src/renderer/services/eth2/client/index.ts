d886913b3c4fe24edcf02f1c7f22b7c5d0e0feeb
Bernard <bero4net@gmail.com>
2021-01-12 10:45:17 +0100

    fix typo

--- src/renderer/services/eth2/client/index.ts
+++ src/renderer/services/eth2/client/index.ts
@@ -25,7 +25,7 @@ export function getEth2ApiClient(url: string, network: string, logger?: ILogger)
 
 export async function readBeaconChainNetwork(url: string): Promise<INetworkConfig | null> {
     // eslint-disable-next-line camelcase
-    type GenesisRequestType = {data: {genesis_fork_version: string}};
-    const genesisResponse = await new HttpClient(url).get<GenesisRequestType>("/eth/v1/beacon/genesis");
+    type GenesisResponseType = {data: {genesis_fork_version: string}};
+    const genesisResponse = await new HttpClient(url).get<GenesisResponseType>("/eth/v1/beacon/genesis");
     return getNetworkConfigByGenesisVersion(genesisResponse.data.genesis_fork_version);
 }
