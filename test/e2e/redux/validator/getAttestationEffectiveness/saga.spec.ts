c821244ce88d155a21c12efe20e1a64c0368f658
Bernard <bero4net@gmail.com>
2021-01-12 12:57:20 +0100

    implement before each in test

--- test/e2e/redux/validator/getAttestationEffectiveness/saga.spec.ts
+++ test/e2e/redux/validator/getAttestationEffectiveness/saga.spec.ts
@@ -1,5 +1,10 @@
-import {testSaga} from "redux-saga-test-plan";
+import {TestApi, testSaga} from "redux-saga-test-plan";
 import {getAttestationEffectiveness} from "../../../../../src/renderer/ducks/validator/sagas";
+import {signedNewAttestation} from "../../../../../src/renderer/ducks/validator/actions";
+import {getValidator} from "../../../../../src/renderer/ducks/validator/selectors";
+import {V4Keystore} from "../../../../../src/renderer/services/keystore";
+import {IValidatorComplete} from "../../../../../src/renderer/ducks/validator/slice";
+import {ValidatorStatus} from "../../../../../src/renderer/constants/validatorStatus";
 
 /** test koraci koji moraju biti pokriveni
  * "normalne" situacije
@@ -34,12 +39,30 @@ import {getAttestationEffectiveness} from "../../../../../src/renderer/ducks/val
  *  - () kad nakon zadnjeg poznatog bloka nalazi se preskocena atterstatcija tipa (...end, null, null, empty...)
  * */
 
-describe("auth sagas", () => {
-    it("authorize saga whit user in DB", () => {
-        testSaga(getAttestationEffectiveness)
-            .next()
-            .put()
-            .next()
-            .isDone();
+const publicKey = "0x9331f1ec6672748ca7b080faff7038da35838f57d223db4f2cb5020246e6c31695c3fb3db0d78db13d266476e34e4e65";
+
+const selectedValidator: IValidatorComplete = {
+    balance: BigInt(31917137053),
+    beaconNodes: ["http://localhost:5052"],
+    isRunning: true,
+    keystore: undefined as V4Keystore,
+    name: "Validator Because Link",
+    network: "pyrmont",
+    status: ValidatorStatus.ACTIVE,
+    publicKey,
+};
+
+const AFTER_INITIALIZATION = "afterInitialization";
+
+describe("getAttestationEffectiveness", () => {
+    let saga: TestApi;
+
+    beforeEach(() => {
+        saga = testSaga(getAttestationEffectiveness, signedNewAttestation(publicKey, "b", 1, 2));
+        saga.next().select(getValidator, {publicKey}).next(selectedValidator).save(AFTER_INITIALIZATION);
+    });
+
+    it("case: A", () => {
+        saga.next();
     });
 });
