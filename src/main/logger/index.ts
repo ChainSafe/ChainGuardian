c1787abfbb3401a9b479efc58b3b48bd2133e4a6
Bernard <bero4net@gmail.com>
2021-01-14 13:44:58 +0100

    improve main logger

--- src/main/logger/index.ts
+++ src/main/logger/index.ts
@@ -1,1 +1,14 @@
-export * from "./lodestar";
+import electronLog from "electron-log";
+import {app} from "electron";
+
+const createLogger = (name: string, file: string): electronLog.ElectronLog => {
+    const logger = electronLog.create(name);
+    logger.transports.file.fileName = file;
+
+    if (process.env.NODE_ENV === "production" || !app.commandLine.hasSwitch("enable-devtools"))
+        logger.transports.console.level = false;
+
+    return logger;
+};
+
+export const mainLogger = createLogger("mainLogger", "core.log");
