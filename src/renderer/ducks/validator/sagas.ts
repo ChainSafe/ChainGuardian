f2689041b22584a1fdf69800c970368df0627c74
Bernard <bero4net@gmail.com>
2021-01-19 13:34:19 +0100

    remove validator balance history on remove

--- src/renderer/ducks/validator/sagas.ts
+++ src/renderer/ducks/validator/sagas.ts
@@ -139,13 +139,15 @@ export function* addNewValidatorSaga(action: ReturnType<typeof addNewValidator>)
 
 function* removeValidatorSaga(
     action: ReturnType<typeof removeActiveValidator>,
-): Generator<SelectEffect | PutEffect, void, CGAccount | null> {
+): Generator<SelectEffect | PutEffect | Promise<void>, void, CGAccount | null> {
     cgLogger.info("Removing validator", action.payload);
+    yield put(unsubscribeToBlockListening(action.payload));
+
     const auth: CGAccount | null = yield select(getAuthAccount);
     deleteKeystore(auth.directory, action.payload);
     auth.removeValidator(action.meta);
 
-    yield put(unsubscribeToBlockListening(action.payload));
+    yield database.validator.balance.delete(action.payload);
     yield put(removeValidator(action.payload));
 }
 
