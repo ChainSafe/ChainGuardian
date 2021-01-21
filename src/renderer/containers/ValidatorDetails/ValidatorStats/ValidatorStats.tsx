5f15f3c6387762872d12c8c8840d9e1b4705f9fb
Bernard <bero4net@gmail.com>
2021-01-21 15:33:26 +0100

    implement date to validator balance chart

--- src/renderer/containers/ValidatorDetails/ValidatorStats/ValidatorStats.tsx
+++ src/renderer/containers/ValidatorDetails/ValidatorStats/ValidatorStats.tsx
@@ -2,18 +2,22 @@ import React, {ReactElement, useEffect, useState} from "react";
 import {ButtonSecondary} from "../../../components/Button/ButtonStandard";
 import {IValidator} from "../../../ducks/validator/slice";
 import {SimpleLineChart, SimpleLineChartRecord} from "../../../components/SimpleLineChart/SimpleLineChart";
-import {ResponsiveContainer} from "recharts";
+import {ResponsiveContainer, TickFormatterFunction} from "recharts";
 import database from "../../../services/db/api/database";
 import {utils} from "ethers";
 import {shell} from "electron";
 import ReactTooltip from "react-tooltip";
+import {Switch} from "../../../components/Switch/Switch";
+import {getValidatorBalanceChartData} from "../../../services/utils/charts";
 
 interface IValidatorStatsProps {
     validator: IValidator;
 }
 
 export const ValidatorStats = ({validator}: IValidatorStatsProps): ReactElement => {
-    const [data, setData] = useState<SimpleLineChartRecord[]>([]);
+    const [epochData, setEpochData] = useState<SimpleLineChartRecord[]>([]);
+    const [dateData, setDateData] = useState<SimpleLineChartRecord[]>([]);
+    const [useDate, setUseData] = useState(false);
 
     const onBeaconChainClick = (): void => {
         const network = validator.network !== "mainnet" ? validator.network + "." : "";
@@ -25,7 +29,9 @@ export const ValidatorStats = ({validator}: IValidatorStatsProps): ReactElement 
         const intervalFn = async (): Promise<void> => {
             const balances = await database.validator.balance.get(validator.publicKey);
 
-            setData(balances.records.map(({balance, epoch}) => ({label: String(epoch), value: Number(balance)})));
+            const {epoch, date} = getValidatorBalanceChartData(balances.records, validator.network);
+            setEpochData(epoch);
+            setDateData(date);
         };
         intervalFn();
         const interval = setInterval(intervalFn, 2 * 60 * 1000);
@@ -38,7 +44,20 @@ export const ValidatorStats = ({validator}: IValidatorStatsProps): ReactElement 
         utils.formatEther(utils.parseUnits(value.toString(), "gwei")),
         "ETH",
     ];
-    const labelFormatter = (label: string | number): string => `Epoch ${label}`;
+    const labelFormatter = (label: string | number): React.ReactNode => {
+        const index = (useDate ? dateData : epochData).findIndex((value) => value.label === label);
+        if (index !== -1) {
+            return (
+                <span>
+                    Epoch {epochData[index].label}
+                    <br />
+                    {dateData[index].label}
+                </span>
+            );
+        }
+        return "unknown";
+    };
+    const tickFormatter: TickFormatterFunction = (tick: string) => (useDate ? tick.slice(0, -6) : tick);
 
     return (
         <div className='validator-details-stats'>
@@ -53,15 +72,18 @@ export const ValidatorStats = ({validator}: IValidatorStatsProps): ReactElement 
             <div className='node-graph-container' style={{width: "100%"}}>
                 <div className='graph-header'>
                     <div className='graph-title'>Validator Balance</div>
+                    <Switch checked={useDate} onChange={setUseData} offLabel='Epoch' onLabel='Date' />
                 </div>
                 <div className='graph-content'>
                     <ResponsiveContainer width='100%' height={200}>
                         <SimpleLineChart
-                            data={data}
+                            data={useDate ? dateData : epochData}
                             tooltip={{formatter, labelFormatter, separator: " "}}
                             xAxis={{
                                 height: 40,
-                                label: {value: "epoch", position: "insideBottom"},
+                                label: {value: useDate ? "date" : "epoch", position: "insideBottom"},
+                                interval: "preserveStartEnd",
+                                tickFormatter,
                             }}
                             yAxis={{
                                 hide: false,
