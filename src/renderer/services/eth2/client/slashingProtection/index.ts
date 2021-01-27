d033e561a994633856a5a131b3bfa5aa3c4ac1c1
Bernard <bero4net@gmail.com>
2021-01-27 14:20:55 +0100

    fix comments

--- src/renderer/services/eth2/client/slashingProtection/index.ts
+++ src/renderer/services/eth2/client/slashingProtection/index.ts
@@ -1,11 +1,11 @@
 import {SlashingProtection} from "@chainsafe/lodestar-validator";
 import {Interchange, InterchangeFormatVersion} from "@chainsafe/lodestar-validator/lib/slashingProtection/interchange";
 import {Root} from "@chainsafe/lodestar-types";
+import {fromHex} from "@chainsafe/lodestar-utils";
 
 export class CGSlashingProtection extends SlashingProtection {
     public missingImportedSlashingProtection = async (publicKey: string): Promise<boolean> => {
-        const uintPublicKey = new Uint8Array(Buffer.from(publicKey.substr(2), "hex"));
-
+        const uintPublicKey = fromHex(publicKey);
         // @ts-ignore
         const signedBlocks = await this.blockService.exportBlocks(uintPublicKey);
         // @ts-ignore
@@ -15,8 +15,7 @@ export class CGSlashingProtection extends SlashingProtection {
     };
 
     public exportSlashingJson = (genesisValidatorsRoot: Root, publicKey: string): Promise<Interchange> => {
-        const uintPublicKey = new Uint8Array(Buffer.from(publicKey.substr(2), "hex"));
-
+        const uintPublicKey = fromHex(publicKey);
         // for now is hardcoded as only option for exporting db
         const interchangeFormatVersion: InterchangeFormatVersion = {
             format: "complete",
