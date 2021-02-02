8acab63a5fc496d6fd4d2e52f2a5f345fde94206
Bernard <bero4net@gmail.com>
2021-02-02 12:52:10 +0100

    fix tests

--- test/e2e/redux/auth/sagas.spec.ts
+++ test/e2e/redux/auth/sagas.spec.ts
@@ -3,10 +3,11 @@ import {authorize} from "../../../../src/renderer/ducks/auth/sagas";
 import {storeAuth} from "../../../../src/renderer/ducks/auth/actions";
 import {CGAccount} from "../../../../src/renderer/models/account";
 import {loadValidatorsAction} from "../../../../src/renderer/ducks/validator/actions";
+import {setInitialValidators} from "../../../../src/renderer/ducks/settings/actions";
 
 describe("auth sagas", () => {
     it("authorize saga without user in DB", () => {
-        testSaga(authorize).next().next(null).isDone();
+        testSaga(authorize).next().next(null).put(setInitialValidators(false)).next().isDone();
     });
 
     it("authorize saga whit user in DB", () => {
