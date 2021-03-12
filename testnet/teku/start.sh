ae50571a55e3fba31f2f6cbe55f276dd299fcae0
Bernard <bero4net@gmail.com>
2021-03-12 17:28:48 +0100

    attempt to run teku docker

---
+++ testnet/teku/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+START_DELAY=30
+CURRENT_TIME=$(date +%s)
+GENESIS_TIME=$((CURRENT_TIME + START_DELAY))
+
+java -version
+
+exec /opt/teku/bin/teku \
+ --network=minimal \
+ --rest-api-enabled \
+ --rest-api-cors-origins=http://localhost:2003 \
+ --rest-api-host-allowlist=* \
+ --log-file=/data/teku.log \
+ --data-path=/data \
+ --data-storage-mode=archive \
+ --Xinterop-genesis-time=${GENESIS_TIME} \
+ --Xinterop-owned-validator-start-index=0 \
+ --Xinterop-owned-validator-count=8 \
+ --Xinterop-number-of-validators=8 \
+ --Xinterop-enabled \
+ --p2p-enabled=false \
+ --Xlog-wire-cipher-enabled \
+ --Xlog-wire-mux-enabled \
+ --Xlog-wire-gossip-enabled
