de997b4dbebf213bd0a9e77d4a08c5ad408bcbea
Bernard <bero4net@gmail.com>
2021-03-18 11:34:14 +0100

    implement validator tests

---
+++ test/validator/lighthouse.ts
@@ -0,0 +1,20 @@
+import {SecretKey} from "@chainsafe/bls";
+import {Keystore} from "@chainsafe/bls-keystore";
+import keystore from "./lighthouse-keystore.json";
+import assert from "assert";
+
+import {restValidation} from "./restValidation";
+const sk = "222222222222222222222222222222222222222222222222222";
+
+(async function (): Promise<void> {
+    const {proposer, attestation} = await restValidation({
+        baseUrl: "http://localhost:5052",
+        getValidatorPrivateKey: async () => SecretKey.fromBytes(await Keystore.fromObject(keystore).decrypt(sk)),
+        limit: 5,
+    });
+
+    assert.equal(proposer.proposed, proposer.delegated);
+    assert.equal(attestation.attestations, attestation.delegated);
+
+    process.exit();
+})();
---
+++ test/validator/teku.ts
@@ -0,0 +1,16 @@
+import {getInteropKey} from "../../src/renderer/services/validator/interop_keys";
+import assert from "assert";
+import {restValidation} from "./restValidation";
+
+(async function (): Promise<void> {
+    const {proposer, attestation} = await restValidation({
+        baseUrl: "http://localhost:5051",
+        getValidatorPrivateKey: async () => getInteropKey(7),
+        limit: 5,
+    });
+
+    assert.equal(proposer.proposed, proposer.delegated);
+    assert.equal(attestation.attestations, attestation.delegated);
+
+    process.exit();
+})();
