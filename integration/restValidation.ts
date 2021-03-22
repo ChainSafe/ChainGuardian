342ceee1626202cff56a503e2b33569a35b2b3e6
Bernard <bero4net@gmail.com>
2021-03-22 13:05:22 +0100

    refactor

--- integration/restValidation.ts
+++ integration/restValidation.ts
@@ -8,20 +8,7 @@ import sinon from "sinon";
 import {computeEpochAtSlot} from "@chainsafe/lodestar-beacon-state-transition";
 import {BeaconCommitteeResponse, BLSPubkey, ProposerDuty} from "@chainsafe/lodestar-types";
 import {AttestationEvent, CGBeaconEventType, ICgEth2ApiClient} from "../src/renderer/services/eth2/client/interface";
-import {List} from "@chainsafe/ssz";
-import {BeaconEventType, HeadEvent} from "@chainsafe/lodestar-validator/lib/api/interface/events";
-
-const getValidatorIndexInCommitteeFromBits = (aggregationBits: List<boolean>): number => {
-    let firstIndex = -1;
-    aggregationBits.forEach((bool, index) => {
-        if (bool) {
-            // in case aggregate and proof we dont want to use index
-            if (firstIndex !== -1) return -1;
-            firstIndex = index;
-        }
-    });
-    return firstIndex;
-};
+import {BeaconBlockEvent, BeaconEventType} from "@chainsafe/lodestar-validator/lib/api/interface/events";
 
 const getCommitteesFactory = (apiClient: ICgEth2ApiClient) => async (
     validatorIndex: number,
@@ -39,10 +26,64 @@ const getCommitteesFactory = (apiClient: ICgEth2ApiClient) => async (
 const getProposerFactory = (apiClient: ICgEth2ApiClient) => async (
     pubKey: BLSPubkey,
     epoch: number,
-): Promise<ProposerDuty[]> => await apiClient.validator.getProposerDuties(epoch, [pubKey]);
+    index: number,
+    ignoreBefore?: number,
+): Promise<ProposerDuty[]> => {
+    const result = await apiClient.validator.getProposerDuties(epoch, [pubKey]);
+    return [...result].filter(
+        ({validatorIndex, slot}) => validatorIndex === index && (slot > ignoreBefore || ignoreBefore === undefined),
+    );
+};
 
-const isBlockProposedFactory = (apiClient: ICgEth2ApiClient) => async (slot: number): Promise<boolean> =>
-    !!(await apiClient.beacon.blocks.getBlockAttestations(slot));
+const processAttestation = (
+    {data}: AttestationEvent["message"],
+    committees: BeaconCommitteeResponse[],
+    attestations: Map<number, boolean>,
+): void => {
+    const committee = committees.find(({slot, index}) => slot === data.slot && index === data.index);
+    if (committee) {
+        attestations.set(committee.slot, true);
+    }
+};
+
+const processBlock = async (
+    {slot}: BeaconBlockEvent["message"],
+    lastEpoch: number,
+    firstSlot: number,
+    validatorPublicKeyBytes: Uint8Array,
+    validatorIndex: number,
+    proposers: Map<number, boolean>,
+    attestations: Map<number, boolean>,
+    committees: BeaconCommitteeResponse[],
+    getProposer: ReturnType<typeof getProposerFactory>,
+    getCommittees: ReturnType<typeof getCommitteesFactory>,
+    limit: number,
+): Promise<{
+    epoch: number;
+    lastEpoch: number;
+    committees: BeaconCommitteeResponse[];
+}> => {
+    if (proposers.has(slot)) proposers.set(slot, true);
+
+    const epoch = computeEpochAtSlot(config, slot);
+    if (lastEpoch !== epoch && limit !== epoch) {
+        // eslint-disable-next-line no-param-reassign
+        lastEpoch = epoch;
+
+        const proposerResponse = await getProposer(validatorPublicKeyBytes, epoch, validatorIndex, firstSlot);
+        proposerResponse.forEach(({slot: s}) => {
+            proposers.set(s, false);
+        });
+
+        // eslint-disable-next-line no-param-reassign
+        committees = await getCommittees(validatorIndex, slot, firstSlot);
+        committees.forEach(({slot: s}) => {
+            attestations.set(s, false);
+        });
+    }
+
+    return {epoch, lastEpoch, committees};
+};
 
 export const restValidation = ({
     baseUrl,
@@ -87,75 +128,72 @@ export const restValidation = ({
             await validatorService.start();
 
             let firstSlot: number | undefined;
-            let slot: number | undefined;
 
             let startEpoch = 1;
             let lastEpoch = 1;
+
+            const onFirstBlock = ({slot}: BeaconBlockEvent["message"]): void => {
+                const epoch = computeEpochAtSlot(config, slot);
+                if (!firstSlot) {
+                    firstSlot = slot;
+                    startEpoch = epoch;
+                }
+            };
 
             const getCommittees = getCommitteesFactory(eth2API);
             let committees: BeaconCommitteeResponse[] = [];
             const attestations = new Map<number, boolean>();
 
             const getProposer = getProposerFactory(eth2API);
-            const isBlockProposed = isBlockProposedFactory(eth2API);
             const proposers = new Map<number, boolean>();
 
             const stream = await eth2API.events.getEventStream(([
-                CGBeaconEventType.HEAD,
+                CGBeaconEventType.BLOCK,
                 CGBeaconEventType.ATTESTATION,
             ] as unknown) as BeaconEventType[]);
             for await (const {type, message} of stream) {
-                if (((type as unknown) as CGBeaconEventType.ATTESTATION) === CGBeaconEventType.ATTESTATION) {
-                    const {data, aggregationBits} = (message as unknown) as AttestationEvent["message"];
-                    const committee = committees.find(({slot, index}) => slot === data.slot && index === data.index);
-                    if (committee) {
-                        const bitsIndex = getValidatorIndexInCommitteeFromBits(aggregationBits);
-                        if (bitsIndex === bitsIndex) {
-                            attestations.set(committee.slot, true);
+                switch ((type as unknown) as CGBeaconEventType) {
+                    case CGBeaconEventType.ATTESTATION: {
+                        processAttestation(
+                            (message as unknown) as AttestationEvent["message"],
+                            committees,
+                            attestations,
+                        );
+                        break;
+                    }
+                    case CGBeaconEventType.BLOCK: {
+                        onFirstBlock((message as unknown) as BeaconBlockEvent["message"]);
+                        const {epoch, ...rest} = await processBlock(
+                            (message as unknown) as BeaconBlockEvent["message"],
+                            lastEpoch,
+                            firstSlot,
+                            validatorPublicKeyBytes,
+                            validator.index,
+                            proposers,
+                            attestations,
+                            committees,
+                            getProposer,
+                            getCommittees,
+                            startEpoch + limit,
+                        );
+
+                        lastEpoch = rest.lastEpoch;
+                        committees = rest.committees;
+
+                        if (startEpoch + limit === epoch) {
+                            await validatorService.stop();
+                            resolve({
+                                proposer: {
+                                    proposed: [...proposers.values()].reduce((p, c) => p + Number(c), 0),
+                                    delegated: proposers.size,
+                                },
+                                attestation: {
+                                    attestations: [...attestations.values()].reduce((p, c) => p + Number(c), 0),
+                                    delegated: attestations.size,
+                                },
+                            });
                         }
-                    }
-                }
-                if (((type as unknown) as CGBeaconEventType.HEAD) === CGBeaconEventType.HEAD) {
-                    slot = (message as HeadEvent["message"]).slot;
-                    const previousSlot = slot - 1;
-                    if (proposers.has(previousSlot)) {
-                        isBlockProposed(previousSlot).then((b) => {
-                            proposers.set(previousSlot, b);
-                        });
-                    }
-
-                    const epoch = computeEpochAtSlot(config, slot);
-                    if (!firstSlot) {
-                        firstSlot = slot;
-                        startEpoch = epoch;
-                    }
-
-                    if (startEpoch + limit === epoch) {
-                        await validatorService.stop();
-                        resolve({
-                            proposer: {
-                                proposed: [...proposers.values()].reduce((p, c) => p + Number(c), 0),
-                                delegated: proposers.size,
-                            },
-                            attestation: {
-                                attestations: [...attestations.values()].reduce((p, c) => p + Number(c), 0),
-                                delegated: attestations.size,
-                            },
-                        });
-                    } else if (lastEpoch !== epoch) {
-                        lastEpoch = epoch;
-                        getProposer(validatorPublicKeyBytes, epoch).then((p) => {
-                            p.forEach(({slot: s}) => {
-                                proposers.set(s, false);
-                            });
-                        });
-
-                        getCommittees(validator.index, slot, firstSlot).then((c) => {
-                            committees = c;
-                            c.forEach(({slot: s}) => {
-                                attestations.set(s, false);
-                            });
-                        });
+                        break;
                     }
                 }
             }
