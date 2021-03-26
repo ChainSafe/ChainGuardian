f237e732ea8ae192f4600752d2659ac781052e16
Bernard <bero4net@gmail.com>
2021-03-26 11:09:13 +0100

    update teku version

--- src/renderer/services/eth2/client/teku/index.ts
+++ src/renderer/services/eth2/client/teku/index.ts
@@ -1,16 +1,3 @@
 import {CgEth2ApiClient} from "../module";
-import {IBeaconConfig} from "@chainsafe/lodestar-config";
-import {Dispatch} from "redux";
-import {IEventsApi} from "@chainsafe/lodestar-validator/lib/api/interface/events";
-import {CgTekuEth2EventsApi} from "./cgTekuEth2EventsApi";
 
-export class CgTekuEth2Api extends CgEth2ApiClient {
-    public constructor(
-        config: IBeaconConfig,
-        url: string,
-        {publicKey, dispatch}: {publicKey?: string; dispatch?: Dispatch} = {},
-    ) {
-        super(config, url, {publicKey, dispatch});
-        this.events = (new CgTekuEth2EventsApi(config, url) as unknown) as IEventsApi;
-    }
-}
+export class CgTekuEth2Api extends CgEth2ApiClient {}
