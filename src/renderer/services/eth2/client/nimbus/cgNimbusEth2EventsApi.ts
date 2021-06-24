03ed6f0c5eca660056e6d7dd46be3c950e64b633
Bernard <bero4net@gmail.com>
2021-06-24 10:40:07 +0200

    implement nimbus events

--- src/renderer/services/eth2/client/nimbus/cgNimbusEth2EventsApi.ts
+++ src/renderer/services/eth2/client/nimbus/cgNimbusEth2EventsApi.ts
@@ -1,8 +1,194 @@
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {CgEth2EventsApi} from "../eth2ApiClient/cgEth2EventsApi";
+import {CGBeaconEvent, CGBeaconEventType, ErrorEvent} from "../interface";
+import {IStoppableEventIterable, LodestarEventIterator} from "@chainsafe/lodestar-utils";
+import {HttpClient} from "../../../api";
+import {computeEpochAtSlot, computeStartSlotAtEpoch} from "@chainsafe/lodestar-beacon-state-transition/lib/util/epoch";
+import {BeaconEventType} from "@chainsafe/lodestar-validator/lib/api/interface/events";
+
+type BlockState = {
+    slot: number;
+    epoch: number;
+    state: string;
+};
+
+type BlockResponse = {
+    data: {
+        root: string;
+        canonical: boolean;
+        header: {
+            message: {
+                slot: string;
+                // eslint-disable-next-line camelcase
+                proposer_index: string;
+                // eslint-disable-next-line camelcase
+                parent_root: string;
+                // eslint-disable-next-line camelcase
+                state_root: string;
+                // eslint-disable-next-line camelcase
+                body_root: string;
+            };
+            signature: string;
+        };
+    };
+};
+
+type PushedAttestation = {
+    slot: number;
+    index: number;
+    beaconBlockRoot: string;
+};
+
+type Attestation = {
+    // eslint-disable-next-line camelcase
+    aggregation_bits: string;
+    data: {
+        slot: string;
+        index: string;
+        // eslint-disable-next-line camelcase
+        beacon_block_root: string;
+        source: {
+            epoch: string;
+            root: string;
+        };
+        target: {
+            epoch: string;
+            root: string;
+        };
+    };
+    signature: string;
+};
 
 export class CgNimbusEth2EventsApi extends CgEth2EventsApi {
+    private currentBlock: BlockState;
+    private depend: BlockState;
+    private previousDepend: BlockState;
+    private pushedAttestation: PushedAttestation[] = [];
+
     public constructor(config: IBeaconConfig, baseUrl: string) {
         super(config, baseUrl);
     }
+
+    // topics: head, block, attestation, voluntary_exit, finalized_checkpoint, chain_reorg
+    public getEventStream = (topics: CGBeaconEventType[]): IStoppableEventIterable<CGBeaconEvent | ErrorEvent> => {
+        const httpClient = new HttpClient(this.baseUrl + "/eth/v1/");
+        return new LodestarEventIterator(({push}): (() => void) => {
+            let interval: NodeJS.Timeout;
+            (async (): Promise<void> => {
+                await this.initializeEventPooling(httpClient);
+
+                interval = setInterval(async () => {
+                    const result = await httpClient.get<BlockResponse>("beacon/headers/head");
+                    const slot = Number(result.data.header.message.slot);
+
+                    if (this.currentBlock.slot !== slot) {
+                        this.pushedAttestation = [];
+
+                        if (topics.some((topic) => topic === CGBeaconEventType.BLOCK))
+                            push({
+                                type: BeaconEventType.BLOCK,
+                                message: this.config.types.BlockEventPayload.fromJson({
+                                    slot,
+                                    block: result.data.root,
+                                }),
+                            });
+
+                        const epoch = computeEpochAtSlot(this.config, slot);
+                        if (this.currentBlock.epoch !== epoch) {
+                            this.previousDepend = this.depend;
+                            this.depend = this.currentBlock;
+                        }
+
+                        if (topics.some((topic) => topic === CGBeaconEventType.HEAD))
+                            // root => block
+                            // state => header.message.state_root
+                            // epoch_transition => on epoch changed
+                            // TODO: ⇩ implement after ssz add this fields ⇩
+                            // previous_duty_dependent_root => if epoch_transition then state before
+                            // current_duty_dependent_root => if epoch_transition then state before
+                            push({
+                                type: BeaconEventType.HEAD,
+                                message: this.config.types.ChainHead.fromJson({
+                                    slot,
+                                    block: result.data.root,
+                                    state: result.data.header.message.state_root,
+                                    epochTransition: this.currentBlock.epoch !== epoch,
+                                }),
+                            });
+
+                        this.currentBlock = {
+                            slot,
+                            epoch,
+                            state: result.data.root,
+                        };
+                    }
+
+                    if (topics.some((topic) => topic === CGBeaconEventType.ATTESTATION)) {
+                        const attestations = await httpClient.get<{data: Attestation[]}>(
+                            `beacon/pool/attestations?slot=${slot}`,
+                        );
+                        for (const attestation of attestations.data) {
+                            const slot = Number(attestation.data.slot);
+                            const index = Number(attestation.data.index);
+                            if (
+                                !this.pushedAttestation.some(
+                                    (pushed) =>
+                                        pushed.slot === slot &&
+                                        pushed.index === index &&
+                                        pushed.beaconBlockRoot === attestation.data.beacon_block_root,
+                                )
+                            ) {
+                                this.pushedAttestation.push({
+                                    slot,
+                                    index,
+                                    beaconBlockRoot: attestation.data.beacon_block_root,
+                                });
+                                push({
+                                    type: CGBeaconEventType.ATTESTATION,
+                                    message: this.config.types.Attestation.fromJson(attestation, {case: "snake"}),
+                                });
+                            }
+                        }
+                    }
+                }, 1000);
+            })();
+            return (): void => {
+                if (interval) clearInterval(interval);
+            };
+        });
+    };
+
+    private initializeEventPooling = async (httpClient: HttpClient): Promise<void> => {
+        if (!this.currentBlock || !this.depend || !this.previousDepend) {
+            if (!this.currentBlock) {
+                const result = await httpClient.get<BlockResponse>("beacon/headers/head");
+                const slot = Number(result.data.header.message.slot);
+                this.currentBlock = {
+                    slot,
+                    epoch: computeEpochAtSlot(this.config, slot),
+                    state: result.data.root,
+                };
+            }
+            if (!this.depend) {
+                const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.currentBlock.epoch) - 1;
+                const result = await httpClient.get<BlockResponse>(`beacon/headers/${previousEpochLastSlot}`);
+                const slot = Number(result.data.header.message.slot);
+                this.depend = {
+                    slot,
+                    epoch: computeEpochAtSlot(this.config, slot),
+                    state: result.data.root,
+                };
+            }
+            if (!this.previousDepend) {
+                const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.depend.epoch) - 1;
+                const result = await httpClient.get<BlockResponse>(`beacon/headers/${previousEpochLastSlot}`);
+                const slot = Number(result.data.header.message.slot);
+                this.previousDepend = {
+                    slot,
+                    epoch: computeEpochAtSlot(this.config, slot),
+                    state: result.data.root,
+                };
+            }
+        }
+    };
 }
--- src/renderer/services/eth2/client/nimbus/cgNimbusEth2ValidatorApi.ts
+++ src/renderer/services/eth2/client/nimbus/cgNimbusEth2ValidatorApi.ts
@@ -2,7 +2,6 @@ import {CgEth2ValidatorApi} from "../eth2ApiClient/cgEth2ValidatorApi";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {HttpClient} from "../../../api";
 import {CommitteeIndex, Slot, ValidatorIndex} from "@chainsafe/lodestar-types";
-import {cgLogger} from "../../../../../main/logger";
 
 export class CgNimbusEth2ValidatorApi extends CgEth2ValidatorApi {
     public constructor(config: IBeaconConfig, httpClient: HttpClient) {
@@ -10,36 +9,17 @@ export class CgNimbusEth2ValidatorApi extends CgEth2ValidatorApi {
     }
 
     public prepareBeaconCommitteeSubnet = async (
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         validatorIndex: ValidatorIndex,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         committeeIndex: CommitteeIndex,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         committeesAtSlot: number,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         slot: Slot,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         isAggregator: boolean,
     ): Promise<void> => {
-        const params = [
-            committeeIndex,
-            slot,
-            isAggregator,
-            // Hardoded localtestnet adress
-            "0x8309a5281dd43297a3eccc1e70553831ec4dbf6a0b5b38fc910b49fb4eecf37075c90269443fd5fc6b8cad701e3eec53",
-            // TODO: missing param `slot_signature`;
-            // eslint-disable-next-line max-len
-            // https://github.com/status-im/nimbus-eth2/blob/44f652f704f14c67e648f2b14dee652d44b858c5/beacon_chain/rpc/validator_api.nim#L122
-            "",
-        ];
-
-        cgLogger.log(params);
-
-        const client = new HttpClient("http://localhost:9190");
-        const response = await client.get("", {
-            data: {
-                jsonrpc: "2.0",
-                method: "post_v1_validator_beacon_committee_subscriptions",
-                params,
-                id: 1,
-            },
-        });
-
-        cgLogger.log(JSON.stringify(response));
+        // Intentionally left empty!
     };
 }
