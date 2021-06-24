81e2c0ab5fb5f6351b18e16a62fcccb18cf31404
Bernard <bero4net@gmail.com>
2021-06-24 14:41:57 +0200

    implement nimbus events error handling

--- src/renderer/services/eth2/client/nimbus/cgNimbusEth2EventsApi.ts
+++ src/renderer/services/eth2/client/nimbus/cgNimbusEth2EventsApi.ts
@@ -5,6 +5,7 @@ import {IStoppableEventIterable, LodestarEventIterator} from "@chainsafe/lodesta
 import {HttpClient} from "../../../api";
 import {computeEpochAtSlot, computeStartSlotAtEpoch} from "@chainsafe/lodestar-beacon-state-transition/lib/util/epoch";
 import {BeaconEventType} from "@chainsafe/lodestar-validator/lib/api/interface/events";
+import {cgLogger} from "../../../../../main/logger";
 
 type BlockState = {
     slot: number;
@@ -71,84 +72,96 @@ export class CgNimbusEth2EventsApi extends CgEth2EventsApi {
 
     // topics: head, block, attestation, voluntary_exit, finalized_checkpoint, chain_reorg
     public getEventStream = (topics: CGBeaconEventType[]): IStoppableEventIterable<CGBeaconEvent | ErrorEvent> => {
-        const httpClient = new HttpClient(this.baseUrl + "/eth/v1/");
+        const httpClient = new HttpClient(this.baseUrl + "/eth/v1/beacon");
         return new LodestarEventIterator(({push}): (() => void) => {
             let interval: NodeJS.Timeout;
             (async (): Promise<void> => {
-                await this.initializeEventPooling(httpClient);
+                // eslint-disable-next-line no-constant-condition
+                while (true) {
+                    try {
+                        await this.initializeEventPooling(httpClient);
+                        break;
+                    } catch (e) {
+                        cgLogger.warn("Failed to initialize data", e, "\n Retying...");
+                    }
+                }
 
                 interval = setInterval(async () => {
-                    const result = await httpClient.get<BlockResponse>("beacon/headers/head");
-                    const slot = Number(result.data.header.message.slot);
+                    try {
+                        const result = await httpClient.get<BlockResponse>("headers/head");
+                        const slot = Number(result.data.header.message.slot);
 
-                    if (this.currentBlock.slot !== slot) {
-                        this.pushedAttestation = [];
+                        if (this.currentBlock.slot !== slot) {
+                            this.pushedAttestation = [];
 
-                        if (topics.some((topic) => topic === CGBeaconEventType.BLOCK))
-                            push({
-                                type: BeaconEventType.BLOCK,
-                                message: this.config.types.BlockEventPayload.fromJson({
-                                    slot,
-                                    block: result.data.root,
-                                }),
-                            });
+                            if (topics.some((topic) => topic === CGBeaconEventType.BLOCK))
+                                push({
+                                    type: BeaconEventType.BLOCK,
+                                    message: this.config.types.BlockEventPayload.fromJson({
+                                        slot,
+                                        block: result.data.root,
+                                    }),
+                                });
 
-                        const epoch = computeEpochAtSlot(this.config, slot);
-                        if (this.currentBlock.epoch !== epoch) {
-                            this.previousDepend = this.depend;
-                            this.depend = this.currentBlock;
+                            const epoch = computeEpochAtSlot(this.config, slot);
+                            if (this.currentBlock.epoch !== epoch) {
+                                this.previousDepend = this.depend;
+                                this.depend = this.currentBlock;
+                            }
+
+                            if (topics.some((topic) => topic === CGBeaconEventType.HEAD))
+                                // root => block
+                                // state => header.message.state_root
+                                // epoch_transition => on epoch changed
+                                // TODO: ⇩ implement after ssz add this fields ⇩
+                                // previous_duty_dependent_root => if epoch_transition then state before
+                                // current_duty_dependent_root => if epoch_transition then state before
+                                push({
+                                    type: BeaconEventType.HEAD,
+                                    message: this.config.types.ChainHead.fromJson({
+                                        slot,
+                                        block: result.data.root,
+                                        state: result.data.header.message.state_root,
+                                        epochTransition: this.currentBlock.epoch !== epoch,
+                                    }),
+                                });
+
+                            this.currentBlock = {
+                                slot,
+                                epoch,
+                                state: result.data.root,
+                            };
                         }
 
-                        if (topics.some((topic) => topic === CGBeaconEventType.HEAD))
-                            // root => block
-                            // state => header.message.state_root
-                            // epoch_transition => on epoch changed
-                            // TODO: ⇩ implement after ssz add this fields ⇩
-                            // previous_duty_dependent_root => if epoch_transition then state before
-                            // current_duty_dependent_root => if epoch_transition then state before
-                            push({
-                                type: BeaconEventType.HEAD,
-                                message: this.config.types.ChainHead.fromJson({
-                                    slot,
-                                    block: result.data.root,
-                                    state: result.data.header.message.state_root,
-                                    epochTransition: this.currentBlock.epoch !== epoch,
-                                }),
-                            });
-
-                        this.currentBlock = {
-                            slot,
-                            epoch,
-                            state: result.data.root,
-                        };
-                    }
-
-                    if (topics.some((topic) => topic === CGBeaconEventType.ATTESTATION)) {
-                        const attestations = await httpClient.get<{data: Attestation[]}>(
-                            `beacon/pool/attestations?slot=${slot}`,
-                        );
-                        for (const attestation of attestations.data) {
-                            const slot = Number(attestation.data.slot);
-                            const index = Number(attestation.data.index);
-                            if (
-                                !this.pushedAttestation.some(
-                                    (pushed) =>
-                                        pushed.slot === slot &&
-                                        pushed.index === index &&
-                                        pushed.beaconBlockRoot === attestation.data.beacon_block_root,
-                                )
-                            ) {
-                                this.pushedAttestation.push({
-                                    slot,
-                                    index,
-                                    beaconBlockRoot: attestation.data.beacon_block_root,
-                                });
-                                push({
-                                    type: CGBeaconEventType.ATTESTATION,
-                                    message: this.config.types.Attestation.fromJson(attestation, {case: "snake"}),
-                                });
+                        if (topics.some((topic) => topic === CGBeaconEventType.ATTESTATION)) {
+                            const attestations = await httpClient.get<{data: Attestation[]}>(
+                                `pool/attestations?slot=${slot}`,
+                            );
+                            for (const attestation of attestations.data) {
+                                const slot = Number(attestation.data.slot);
+                                const index = Number(attestation.data.index);
+                                if (
+                                    !this.pushedAttestation.some(
+                                        (pushed) =>
+                                            pushed.slot === slot &&
+                                            pushed.index === index &&
+                                            pushed.beaconBlockRoot === attestation.data.beacon_block_root,
+                                    )
+                                ) {
+                                    this.pushedAttestation.push({
+                                        slot,
+                                        index,
+                                        beaconBlockRoot: attestation.data.beacon_block_root,
+                                    });
+                                    push({
+                                        type: CGBeaconEventType.ATTESTATION,
+                                        message: this.config.types.Attestation.fromJson(attestation, {case: "snake"}),
+                                    });
+                                }
                             }
                         }
+                    } catch (e) {
+                        push({type: CGBeaconEventType.ERROR});
                     }
                 }, 1000);
             })();
@@ -161,7 +174,7 @@ export class CgNimbusEth2EventsApi extends CgEth2EventsApi {
     private initializeEventPooling = async (httpClient: HttpClient): Promise<void> => {
         if (!this.currentBlock || !this.depend || !this.previousDepend) {
             if (!this.currentBlock) {
-                const result = await httpClient.get<BlockResponse>("beacon/headers/head");
+                const result = await httpClient.get<BlockResponse>("headers/head");
                 const slot = Number(result.data.header.message.slot);
                 this.currentBlock = {
                     slot,
@@ -170,24 +183,32 @@ export class CgNimbusEth2EventsApi extends CgEth2EventsApi {
                 };
             }
             if (!this.depend) {
-                const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.currentBlock.epoch) - 1;
-                const result = await httpClient.get<BlockResponse>(`beacon/headers/${previousEpochLastSlot}`);
-                const slot = Number(result.data.header.message.slot);
-                this.depend = {
-                    slot,
-                    epoch: computeEpochAtSlot(this.config, slot),
-                    state: result.data.root,
-                };
+                try {
+                    const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.currentBlock.epoch) - 1;
+                    const result = await httpClient.get<BlockResponse>(`headers/${previousEpochLastSlot}`);
+                    const slot = Number(result.data.header.message.slot);
+                    this.depend = {
+                        slot,
+                        epoch: computeEpochAtSlot(this.config, slot),
+                        state: result.data.root,
+                    };
+                } catch (e) {
+                    cgLogger.warn("Event error while fetching depend", e);
+                }
             }
             if (!this.previousDepend) {
-                const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.depend.epoch) - 1;
-                const result = await httpClient.get<BlockResponse>(`beacon/headers/${previousEpochLastSlot}`);
-                const slot = Number(result.data.header.message.slot);
-                this.previousDepend = {
-                    slot,
-                    epoch: computeEpochAtSlot(this.config, slot),
-                    state: result.data.root,
-                };
+                try {
+                    const previousEpochLastSlot = computeStartSlotAtEpoch(this.config, this.depend.epoch) - 1;
+                    const result = await httpClient.get<BlockResponse>(`headers/${previousEpochLastSlot}`);
+                    const slot = Number(result.data.header.message.slot);
+                    this.previousDepend = {
+                        slot,
+                        epoch: computeEpochAtSlot(this.config, slot),
+                        state: result.data.root,
+                    };
+                } catch (e) {
+                    cgLogger.warn("Event error while fetching previousDepend", e);
+                }
             }
         }
     };
