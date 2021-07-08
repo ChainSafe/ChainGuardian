0ee667352aece877a7aad119d10df2f6004d5129
Bernard <bero4net@gmail.com>
2021-07-08 12:01:12 +0200

    refactor duty status

--- src/renderer/constants/dutyStatus.ts
+++ src/renderer/constants/dutyStatus.ts
@@ -1,12 +1,18 @@
 export enum DutyStatus {
-    // dont change order of values
-    unknown,
-    scheduled,
-    missed,
-    orphaned,
-    proposed,
-    attested,
+    unknown = 0,
+    scheduled = 1,
+    missed = 10,
+    orphaned = 11,
+    attested = 100,
+    proposed = 101,
 }
 
-const text = ["Unknown", "Scheduled", "Missed", "Orphaned", "Proposed", "Attested"];
-export const getDutyStatusText = (duty: DutyStatus): string => text[duty];
+const dutyStatus = new Map([
+    [DutyStatus.unknown, "Unknown"],
+    [DutyStatus.scheduled, "Scheduled"],
+    [DutyStatus.missed, "Missed"],
+    [DutyStatus.orphaned, "Orphaned"],
+    [DutyStatus.attested, "Attested"],
+    [DutyStatus.proposed, "Proposed"],
+]);
+export const getDutyStatusText = (duty: DutyStatus): string => dutyStatus.get(duty);
