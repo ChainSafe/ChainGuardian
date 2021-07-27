62fca540813af53379b41004611839de0a1304ff
Bernard <bero4net@gmail.com>
2021-07-27 13:33:33 +0200

    update prysm api

--- src/renderer/services/eth2/client/prysm/CgPrysmEth2BeaconStateApi.ts
+++ src/renderer/services/eth2/client/prysm/CgPrysmEth2BeaconStateApi.ts
@@ -4,27 +4,34 @@ import {HttpClient} from "../../../api";
 import {BeaconCommitteeResponse, BLSPubkey, Fork, ValidatorIndex} from "@chainsafe/lodestar-types";
 import {ICGValidatorResponse} from "../interface";
 import {cgLogger} from "../../../../../main/logger";
-import {base64ToHex, hexToBase64} from "./utils";
+import {base64ToHex, hexToBase64, stringToBase64} from "./utils";
 import logger from "electron-log";
 import {ValidatorStateResponse, ValidatorStatusResponse} from "./types";
+import {Json} from "@chainsafe/ssz";
 
 export class CgPrysmEth2BeaconStateApi extends CgEth2BeaconStateApi {
     public constructor(config: IBeaconConfig, httpClient: HttpClient) {
         super(config, httpClient);
     }
 
-    // TODO: changer mock with real data
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public getFork = async (stateId: "head"): Promise<Fork | null> => {
         try {
-            const forkMock = {
-                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
-                previous_version: "0x00000000",
-                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
-                current_version: "0x00000000",
-                epoch: "0",
-            };
-            return this.config.types.Fork.fromJson(forkMock, {case: "snake"});
+            const state = typeof stateId === "string" ? stringToBase64(stateId) : stringToBase64(String(stateId));
+            const forkResponse = await this.httpClient.get<{
+                data: {
+                    // eslint-disable-next-line camelcase
+                    previous_version: string;
+                    // eslint-disable-next-line camelcase
+                    current_version: string;
+                    epoch: string;
+                };
+            }>(`/eth/v1/beacon/states/${state}/fork`);
+
+            return this.config.types.Fork.fromJson({
+                previousVersion: base64ToHex(forkResponse.data.previous_version),
+                currentVersion: base64ToHex(forkResponse.data.current_version),
+                epoch: forkResponse.data.epoch,
+            });
         } catch (e) {
             logger.error("Failed to fetch head fork version", {error: e.message});
             return null;
@@ -69,12 +76,25 @@ export class CgPrysmEth2BeaconStateApi extends CgEth2BeaconStateApi {
     };
 
     public getLastEpoch = async (): Promise<bigint | null> => {
-        throw new Error("getLastEpoch not implemented");
+        try {
+            const url = `/eth/v1/beacon/states/aGVhZA==/finality_checkpoints`;
+            const finalityCheckpointsResponse = await this.httpClient.get<{
+                // eslint-disable-next-line camelcase
+                data: {previous_justified: {epoch: string}};
+            }>(url);
+            return BigInt(finalityCheckpointsResponse.data.previous_justified.epoch);
+        } catch (e) {
+            logger.error("Failed to fetch finality checkpoints", {error: e.message});
+            return null;
+        }
     };
 
-    // TODO: implement this? required for CG testing
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public getCommittees = async (stateId: "head" | number = "head"): Promise<BeaconCommitteeResponse[]> => {
-        throw new Error("getCommittees not implemented");
+        const state = typeof stateId === "string" ? stringToBase64(stateId) : stringToBase64(String(stateId));
+        const url = `/eth/v1/beacon/states/${state}/committees`;
+        const committeesResponse = await this.httpClient.get<any>(url);
+        return committeesResponse.data.map((data: Json) =>
+            this.config.types.BeaconCommitteeResponse.fromJson(data, {case: "snake"}),
+        );
     };
 }
--- src/renderer/services/eth2/client/prysm/CgPrysmEth2EventsApi.ts
+++ src/renderer/services/eth2/client/prysm/CgPrysmEth2EventsApi.ts
@@ -1,71 +1,106 @@
 import {CgEth2EventsApi} from "../eth2ApiClient/cgEth2EventsApi";
-import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {CGBeaconEvent, CGBeaconEventType, ErrorEvent} from "../interface";
 import {IStoppableEventIterable, LodestarEventIterator} from "@chainsafe/lodestar-utils";
-import {PrysmStreamReader} from "./PrysmStreamReader";
-import {Attestation as PrysmAttestation, SignedBeaconBlock} from "./types";
+import EventSource from "eventsource";
 import {BeaconEventType} from "@chainsafe/lodestar-validator/lib/api/interface/events";
 import {base64ToHex} from "./utils";
-import {Attestation} from "@chainsafe/lodestar-types";
-import {mapAttestation} from "./mapProduceBlockResponseToStandardProduceBlock";
-import {Json} from "@chainsafe/ssz";
+
+type PrysmEventData = {
+    "@type": string;
+    slot: string;
+    block: string;
+    state: string;
+    // eslint-disable-next-line camelcase
+    epoch_transition: boolean;
+    // eslint-disable-next-line camelcase
+    previous_duty_dependent_root: string;
+    // eslint-disable-next-line camelcase
+    current_duty_dependent_root: string;
+    // eslint-disable-next-line camelcase
+    aggregation_bits: string;
+    data: {
+        slot: string;
+        index: string;
+        // eslint-disable-next-line camelcase
+        beacon_block_root: string;
+        source: {
+            epoch: string;
+            root: string;
+        };
+        target: {
+            epoch: string;
+            root: string;
+        };
+    };
+    signature: string;
+};
 
 export class CgPrysmEth2EventsApi extends CgEth2EventsApi {
-    public constructor(config: IBeaconConfig, baseUrl: string) {
-        super(config, baseUrl);
-    }
-
     public getEventStream = (topics: CGBeaconEventType[]): IStoppableEventIterable<CGBeaconEvent | ErrorEvent> => {
-        const streams: {[key: string]: PrysmStreamReader<any>} = {};
+        const topicsQuery = topics.filter((topic) => topic !== "chain_reorg").join("&topics=");
+        const url = new URL(`/eth/v1/events?topics=${topicsQuery}`, this.baseUrl);
+        const eventSource = new EventSource(url.href);
         return new LodestarEventIterator(({push}): (() => void) => {
-            // TODO: implement head
-
-            // BLOCK
-            if (topics.some((topic) => topic === "block")) {
-                const url = new URL(`/eth/v1alpha1/beacon/blocks/stream`, this.baseUrl);
-                streams["block"] = new PrysmStreamReader<SignedBeaconBlock>(url);
-                streams["block"].on("data", (data: SignedBeaconBlock) => {
-                    push({
-                        type: BeaconEventType.BLOCK,
-                        message: this.config.types.BlockEventPayload.fromJson(
-                            {
-                                slot: data.block.slot,
-                                block: base64ToHex(data.block.stateRoot),
-                            },
-                            {case: "snake"},
-                        ),
-                    });
-                });
-            }
-
-            // ATTESTATION
-            if (topics.some((topic) => topic === "attestation")) {
-                const url = new URL(`/eth/v1alpha1/beacon/attestations/stream`, this.baseUrl);
-                streams["attestation"] = new PrysmStreamReader<Attestation, PrysmAttestation>(url, {
-                    transformer: (data): Attestation =>
-                        this.config.types.Attestation.fromJson((mapAttestation(data) as unknown) as Json, {
-                            case: "snake",
-                        }),
-                });
-                streams["attestation"].on("data", (data) => {
-                    push({
-                        type: CGBeaconEventType.ATTESTATION,
-                        message: data,
-                    });
-                });
-            }
-
-            // TODO: implement voluntary_exit
-
-            // TODO: implement finalized_checkpoint
-
-            // TODO: implement chain_reorg
-
+            eventSource.addEventListener("message", this.prysmEventListener(push));
+            eventSource.addEventListener("error", () => {
+                push({type: CGBeaconEventType.ERROR});
+            });
             return (): void => {
-                Object.keys(streams).forEach((key) => {
-                    streams[key].removeAllListeners();
-                });
+                eventSource.close();
             };
         });
+    };
+
+    private prysmEventListener = (push: (value: CGBeaconEvent) => void) => (event: Event): void => {
+        const data: PrysmEventData = JSON.parse(((event as unknown) as {data: string}).data);
+        switch (data["@type"]) {
+            case "type.googleapis.com/ethereum.eth.v1.EventBlock":
+                push({
+                    type: BeaconEventType.BLOCK,
+                    message: this.config.types.BlockEventPayload.fromJson({
+                        slot: data.slot,
+                        block: base64ToHex(data.block),
+                    }),
+                });
+                break;
+            case "type.googleapis.com/ethereum.eth.v1.EventHead":
+                push({
+                    type: BeaconEventType.HEAD,
+                    message: this.config.types.ChainHead.fromJson({
+                        slot: data.slot,
+                        block: base64ToHex(data.block),
+                        state: base64ToHex(data.state),
+                        epochTransition: data.epoch_transition,
+                        previousDutyDependentRoot: base64ToHex(data.previous_duty_dependent_root),
+                        currentDutyDependentRoot: base64ToHex(data.current_duty_dependent_root),
+                    }),
+                });
+                break;
+            case "type.googleapis.com/ethereum.eth.v1.Attestation":
+                push({
+                    type: CGBeaconEventType.ATTESTATION,
+                    message: this.config.types.Attestation.fromJson({
+                        aggregationBits: base64ToHex(data.aggregation_bits),
+                        data: {
+                            slot: data.data.slot,
+                            index: data.data.index,
+                            beaconBlockRoot: base64ToHex(data.data.beacon_block_root),
+                            source: {
+                                epoch: data.data.source.epoch,
+                                root: base64ToHex(data.data.source.root),
+                            },
+                            target: {
+                                epoch: data.data.target.epoch,
+                                root: base64ToHex(data.data.target.root),
+                            },
+                        },
+                        signature: base64ToHex(data.signature),
+                    }),
+                });
+                break;
+            default:
+                console.log(data);
+                throw new Error("Unsupported beacon event type " + data["@type"]);
+        }
     };
 }
