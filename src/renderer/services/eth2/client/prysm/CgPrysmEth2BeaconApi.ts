3cbb985747b49d07f37338774d74b1837c703cde
Bernard <bero4net@gmail.com>
2021-09-30 16:02:11 +0200

    remove console logs

--- src/renderer/services/eth2/client/prysm/CgPrysmEth2BeaconApi.ts
+++ src/renderer/services/eth2/client/prysm/CgPrysmEth2BeaconApi.ts
@@ -4,7 +4,6 @@ import {altair, ssz} from "@chainsafe/lodestar-types";
 export class CgPrysmEth2BeaconApi extends CgEth2BeaconApi {
     public async submitPoolSyncCommitteeSignatures(signatures: altair.SyncCommitteeMessage[]): Promise<void> {
         const data = signatures.map((signature) => ssz.altair.SyncCommitteeMessage.toJson(signature, {case: "snake"}));
-        console.log(data, signatures);
         await this.post("/eth/v1/beacon/pool/sync_committees", data);
     }
 }
--- src/renderer/services/eth2/client/prysm/CgPrysmEth2ValidatorApi.ts
+++ src/renderer/services/eth2/client/prysm/CgPrysmEth2ValidatorApi.ts
@@ -9,24 +9,16 @@ export class CgPrysmEth2ValidatorApi extends CgEth2ValidatorApi {
         epoch: number,
         validatorIndices: ValidatorIndex[],
     ): Promise<{data: SyncDuty[]; dependentRoot: Root}> {
-        console.warn(epoch, validatorIndices);
-
         // eslint-disable-next-line camelcase
         const response = await this.post<Json, {data: Json[]; dependent_root: Json}>(
             `/eth/v1/validator/duties/sync/${epoch}`,
             validatorIndices.map(String),
         );
 
-        console.warn(response.data);
-
         // TODO: remove ts ignore
         // @ts-ignore
         return {
-            data: response.data
-                // @ts-ignore
-                // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
-                .map((data) => ({...data, validator_index: Number(data.validator_index)}))
-                .map((data) => syncDutyContainerType.fromJson(data, {case: "snake"})),
+            data: response.data.map((data) => syncDutyContainerType.fromJson(data, {case: "snake"})),
             // dependentRoot: ssz.Root.fromJson(response.dependent_root),
         };
     }
